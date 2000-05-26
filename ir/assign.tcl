5564d3a29fa7b6fc766250343a52e738cd44b0c8
Christopher Alfeld <calfeld@flux.utah.edu>
2000-05-26 20:57:51 +0000

    assign.tcl - Updated to use assign_hw and 0.3 IR testbed.ptop - Updated to match new assign_hw format.

--- ir/assign.tcl
+++ ir/assign.tcl
@@ -16,7 +16,7 @@
 
 set testbed "testbed.ptop"
 set macs "./macs.txt"
-set assign "../assign/assign"
+set assign "../assign_hw/assign"
 set maxrun 5
 set switchports 32
 
@@ -58,8 +58,7 @@ while {[gets $fp line] >= 0} {
     if {$line == {}} {continue}
     if {$line == "END links"} {break}
 
-    set links([lindex $line 0]) [list [lrange $line 1 2] \
-				     [lrange $line 3 4]]
+    set links([lindex $line 0]) [lrange $line 1 end]
 }
 
 # close the ir for now
@@ -79,8 +78,9 @@ foreach node [array names nodes] {
 # write links
 foreach link [array names links] {
     set src [lindex $links($link) 0]
-    set dst [lindex $links($link) 1]
-    puts $topfp "link [lindex $src 0] [lindex $dst 0]"
+    set dst [lindex $links($link) 2]
+    set bw [string trimright [lindex $links($link) 4] "Mb"]
+    puts $topfp "link $link [lindex $src 0] [lindex $dst 0] $bw"
 }
 
 close $topfp
@@ -92,44 +92,46 @@ set run 0
 while {$run < $maxrun} {
     set assignfp [open "|$assign -b -t $testbed $tmpfile | tee -a [file dirname $argv]/assign.log" r]
     set problem 0
+    set score -1
+    set seed 0
     while {$problem == 0 && [gets $assignfp line] >= 0} {
-	if {[regexp {violated: (.+)} $line match reason] == 1} {
-	    # XXX: add better parsing of reason
-	    puts "$match"
-	    if {[regexp {bandwidth} $reason] != -1} {
-		set problem -1
-	    } else {
-		set problem 1
-	    }
-	} elseif {[regexp {Best solution: ([0-9]+)} $line match score] == 1} {
-	    break
+	if {[regexp {BEST SCORE: ([0-9]+)} $line match score] == 1} {
+	    continue;
+	}
+	if {[regexp {With ([0-9]+) violations} $line match problems] == 1} {
+	    break;
+	}
+	if {[regexp {seed = ([0-9]+)} $line match seed] == 1} {
+	    continue
 	}
     }
-    if {$problem == 1} {
+    if {$problem > 0} {
 	incr run
 	close $assignfp
-	puts "Run $run resulted in violation ($reason)."
+	puts "Run $run resulted in $problem violations."
 	continue
     }
     # we should now be ready to read the solution
+    readto $assignfp "Nodes:"
     while {[gets $assignfp line] >= 0} {
 	if {[regexp {unassigned:} $line] == 1} {
 	    puts stderr "Assign error ($line). I'm confused! (not deleting $tmpfile)"
 	    exit 1
 	}
-	if {$line == "End solution"} {break}
+	if {$line == "End Nodes"} {break}
 	lappend map([lindex $line 1]) [list [lindex $line 0] [lindex $line 2]]
 	set nmap([lindex $line 0]) [lrange $line 1 end]
+    }
+    readto $assignfp "Edges:"
+    while {[gets $assignfp line] >= 0} {
+	if {$line == "End Edges"} {break}
+	set plinks([lindex $line 0]) [lrange $line 1 end]
     }
     break
 }
 if {$run > $maxrun} {
-    if {$problem == -1} {
-	puts "WARNING: switch bandwidth exceeded."
-    } else {
-	puts "Could not find solution (not deleting $tmpfile)!"
-	exit 1
-    }
+    puts "Could not find solution (not deleting $tmpfile)!"
+    exit 1
 }
 close $assignfp
 
@@ -146,6 +148,16 @@ foreach switch [array names map] {
     }
 }
 puts $fp "END nodes"
+puts $fp "START links"
+foreach link [array names plinks] {
+    set pls {}
+    foreach element [lrange $plinks($link) 1 end] {
+	if {[string index $element 0] == "("} {continue}
+	lappend pls $element
+    }
+    puts $fp "$link $pls"
+}
+puts $fp "END links"
 puts $fp "END virtual"
 
 # read macs file
@@ -156,80 +168,52 @@ while {[gets $macfp line] >= 0} {
 }
 close $macfp
 
-# do port computations
-puts "Calculating port numbers"
-
-# pass 1 - here we record all used ports
-foreach node [array names nodes] {
-    foreach link [lrange $nodes($node) 1 end] {
-	if {$node == [lindex [lindex $links($link) 0] 0]} {
-	    set port [lindex [lindex $links($link) 0] 1]
-	    set index 0
-	} else {
-	    set port [lindex [lindex $links($link) 1] 1]
-	    set index 1
-	}
-	if {$port == "-1"} {
-	    # save for pass 2
-	    lappend unresolved [list $link $index $node]
-	} else {
-	    set assigned($node:$port) 1
-	}
-    }
-}
-# pass 2 - here we fill in all -1's from unused numbers
-foreach un $unresolved {
-    set link [lindex $un 0]
-    set index [lindex $un 1]
-    set node [lindex $un 2]
-    # find a free number
-    set nnode [lindex $nmap($node) 0]
-    if {[info exists mac($nnode)] == 0} {
-	# switch
-	set numports $switchports
-    } else {
-	set numports [llength $mac([lindex $nmap($node) 0])]
-    }
-    for {set i 0} {$i < $numports} {incr i} {
-	if {! [info exists assigned($node:$i)]} {
-	    # found one
-	    puts "Port $i on node $node assigned to link $link"
-	    set assigned($node:$i) 1
-	    set links($link) [lreplace $links($link) $index $index \
-				  [list $node $i]]
-	    break
-	}
-    }
-}
-
-# now we apprend the vlan section
+## now we apprend the vlan section
 # first we need to read in macs
 puts "Adding VLAN section"
 puts $fp "START vlan"
 
-# ok, this is harder.  We need to go through every link in links
-# and those that are in the current switch output the correct vlan.
-# We need to be especially careful to get the interlinks right.
-#foreach switch [array names map] {
-#    puts $fp "switch $switch"
-    foreach link [array names links] {
-	set srcp [lindex $links($link) 0]
-	set dstp [lindex $links($link) 1]
-	set src [lindex $srcp 0]
-	set dst [lindex $dstp 0]
-	set srcport [lindex $srcp 1]
-	set dstport [lindex $dstp 1]
-	set srcs [lindex $nmap($src) 0]
-	set dsts [lindex $nmap($dst) 0]
-	if {$srcs == $switch || $dsts == $switch} {
-	    # output a vlan for this
-	    set srcp [lindex $nmap($src) 1]
-	    set dstp [lindex $nmap($dst) 1]
-	    puts $fp "$link [lindex $mac($srcp) $srcport] [lindex $mac($dstp) $dstport]"
+proc getmac {s} {
+    set t [split [string trim $s "()"] ,]
+    if {[lindex $t 0] == "(null)"} {
+	return [lindex $t 1]
+    } else {
+	return [lindex $t 0]
+    }
+}
+
+foreach link [array names plinks] {
+    set t $plinks($link)
+    set type [lindex $t 0]
+    switch $type {
+	"intraswitch" {
+	    # 2 links, but each goes to a switch
+	    set srcmac [getmac [lindex $t 2]]
+	    set dstmac [getmac [lindex $t 4]]
+	    puts $fp "$link $srcmac $dstmac"
+	}
+	"interswitch" {
+	    # 3 or four links
+	    # XXX - in later versions this will become an arbitrary number
+	    # of links.
+	    set ll {}
+	    if {[llength $t] == 8} {set num 4} else {set num 3}
+	    for {set i 0} {$i < $num} {incr i} {
+		set l [lindex $t [expr $i*2+1]]
+		set mac [getmac [lindex $t [expr $i*2+2]]]
+		if {$mac != "(null)"} {
+		    lappend ll $mac
+		}
+	    }
+	    puts $fp "$link $ll"
+	}
+	"direct" {
+	    # 1 link
+	    set macs [split [string trim [lindex $t 2] "()"] ","]
+	    puts $fp "$link $macs"
 	}
     }
-#    puts $fp "end"
-#}
+}
 
 puts $fp "END vlan"
 
