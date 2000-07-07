b97d7d219c00f9025fdefe487c92379a7dc8a4bc
Christopher Alfeld <calfeld@flux.utah.edu>
2000-07-07 23:13:58 +0000

    Initial scans of all.

---
+++ os/oslib.tcl
@@ -0,0 +1,163 @@
+######################################################################
+# oslib.tcl
+#
+# os <command> <args>
+# 
+# init
+# end
+# querydb <node> - Returns a node_state (see below)
+# querynode <node> - Returns a node_state (see below)
+# listbases
+# listsdeltas <base> - Returns list of deltas
+# querybase <base> - Returns {os, ver, extra, desc}
+# querydelta <delta> - Returns {name, desc, path}
+#
+# node_state = {base {deltas}}
+######################################################################
+
+if {[file dirname [info script]] == "."} {
+    load ../lib/sql.so
+} else {
+    load [file dirname [file dirname [info script]]]/lib/sql.so
+}
+
+namespace eval TB_OS {
+
+namespace export os;
+
+variable commands {
+    init end 
+    querydb querynode 
+    listbases 
+    querybase querydelta
+}
+variable DB {}
+
+##################################################
+# os <command> <args>
+# Main interface into this library.  See header for list of
+# commands and args.
+##################################################
+proc os {command args} {
+    variable commands
+    variable DB
+
+    if {[lsearch $commands $command] == -1} {
+	error "$command not a valid os command."
+    }
+    if {$DB == {} && $command != "init"} {
+	error "Need to run 'os init' first."
+    }
+    
+    return [eval "[namespace current]::$command $args"]
+}
+
+##################################################
+# init
+# This initalizes a connection to the database.
+##################################################
+proc init {} {
+    variable DB
+
+    set DB [sql connect]
+    sql selectdb $DB tbdb
+}
+
+##################################################
+# end
+# This disconnects from the database.
+##################################################
+proc end {} {
+    variable DB
+    sql disconnect $DB
+    set DB {}
+}
+
+##################################################
+# querydb <node>
+# Returns the base and deltas for <node>
+##################################################
+proc querydb {node} {
+    variable DB
+
+    sql query $DB "select image_id from SW_table where node_id=\"$node\""
+    set base [sql fetchrow $DB]
+    if {$base == {}} {return {}}
+    sql endquery $DB
+
+    sql query $DB "select fix_id from fixed_list where node_id=\"$node\""
+    set fix_ids {}
+    while {[set row [sql fetchrow $DB]] != {}} {
+	lappend fix_ids $row
+    }
+    sql endquery $DB
+
+    return [list $base $fix_ids]
+}
+
+##################################################
+# querynode <node>
+# Queries the node about the it's state and creates a node_state.
+##################################################
+proc querynode {node} {
+    # XXX - This is non-trivial.  Might use the roatan library.
+}
+
+##################################################
+# listbases
+# Lists all the bases in the DB.
+##################################################
+proc listbases {} {
+    variable DB
+
+    sql query $DB "select image_id from disk_images"
+    set ret {}
+    while {[set row [sql fetchrow $DB]] != {}} {
+	lappend ret $row
+    }
+    sql endquery $DB
+    return $ret
+}
+
+##################################################
+# listdeltas <base>
+# Returns a list of all the deltas for a given base.
+##################################################
+proc listdeltas {base} {
+    variable DB
+
+    sql query $DB "select fix_id from fix_compat where image_id=\"$base\""
+    set ret {}
+    while {[set row [sql fetchrow $DB]] != {}} {
+	lappend ret $row
+    }
+    sql endquery $DB
+    return $ret
+}
+
+##################################################
+# querybase <base>
+# Returns {os, ver, extra, desc} for base.
+##################################################
+proc querybase {base} {
+    variable DB
+
+    sql query $DB "select OS, ver, extras, img_desc from disk_images where image_id=\"$base\""
+    set ret [sql fetchrow $DB]
+    sql endquery $DB
+    return $ret
+}
+
+##################################################
+# querydelta <delta>
+# Returns {name, desc, path}
+##################################################
+proc querydelta {delta} {
+    variable DB
+
+    sql query $DB "select fix_name, fix_desc, fix_path from fixes_table where fix_id=\"$delta\""
+    set ret [sql fetchrow $DB]
+    sql endquery $DB
+    return $ret
+}
+}
---
+++ os/osstatus.tcl
@@ -0,0 +1,53 @@
+#!/usr/local/bin/tclsh
+
+source [file dirname [info script]]/oslib.tcl
+namespace import TB_OS::os
+
+if {[llength $argv] == 0 || [llength $argv] > 2} {
+    puts stderr "Syntax: $arg0 \[-v\[v\]\] <node>"
+    exit 1
+}
+
+os init
+
+set verbose 0
+if {[llength $argv] == 2} {
+    switch -- [lindex $argv 0] {
+	"-v" {set verbose 1}
+	"-vv" {set verbose 2}
+	default {
+	    puts stderr "Unrecognized option: [lindex $argv 0]"
+	    exit 1
+	}
+    }
+    set node [lindex $argv 1]
+} else {
+    set node [lindex $argv 0]
+}
+
+set state [os querydb $node]
+set bstate [os querybase [lindex $state 0]]
+puts "base: [lindex $bstate 3]"
+if {$verbose > 0} {
+    puts "os: [lindex $bstate 0]"
+    puts "ver: [lindex $bstate 1]"
+    puts "extras: [lindex $bstate 2]"
+    puts ""
+}
+puts "deltas: [lindex $state 1]"
+
+if {$verbose > 0} {
+    foreach delta [lindex $state 1] {
+	set info [os querydelta $delta]
+	puts ""
+	puts "delta: $delta"
+	puts "name: [lindex $info 0]"
+	puts "desc: [lindex $info 1]"
+	if {$verbose > 1} {
+	    puts "rpms: [lindex $info 2]"
+	}
+    }
+}
+
+os end
+
