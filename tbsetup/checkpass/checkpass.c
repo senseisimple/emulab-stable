b30ba334e8672e1594851508d1975480542c8c56
Mac Newbold <newbold@flux.utah.edu>
2000-09-20 23:40:41 +0000

    Password Checker - only admits 'good' passwords

---
+++ tbsetup/checkpass/checkpass.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+// From cracklib,2.7/cracklib/fascist.o
+// char* FascistCheck(char* passwd, char* path, char* gecos);
+
+int main(int ARGC, char* ARGV[]) {
+  char gecos[256];
+  char* path = "/usr/local/lib/pw_dict";
+  char passwd[256];
+  char* retval;
+
+  if (ARGC < 4) {
+    printf("Usage: checkpass <pass> <login> <fullname>\n");
+    return 0;
+  }
+
+  strcpy(passwd,ARGV[1]);
+  sprintf(gecos,"%s:%s",ARGV[1],ARGV[3]);
+
+  retval = (char *) FascistCheck(passwd,path,gecos);
+
+  if (retval!=NULL) {
+    printf("Invalid Password: %s\n",retval);
+    return 1;
+  }
+  printf("ok\n");
+  return 0;
+}
