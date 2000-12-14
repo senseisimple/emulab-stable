62551b28cdb364ceceab0fa305cc5f08c2df3597
Leigh B. Stoller <stoller@flux.utah.edu>
2000-12-14 00:37:33 +0000

    Move a bunch of old scripts and stuff to the attic.

--- os/setupmachine.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/csh -x
-sed -e s/NODE/tbpc$1/g setupmachine-client.tmpl >! /tmp/setupmachine-client.sh
-scp /tmp/setupmachine-client.sh 155.99.214.1${1}:/tmp
-ssh 155.99.214.1$1 /bin/csh -x /tmp/setupmachine-client.sh
-rm -f /tmp/setupmachine-client.sh
