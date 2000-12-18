10fef732fb0001ebd2e1d359c6cdabfb572875d7
Christopher Alfeld <calfeld@flux.utah.edu>
2000-12-18 21:34:52 +0000

    Changed to call resetvlans.pl instead of resetvlans.tcl.

--- tbsetup/tbend.tcl
+++ tbsetup/tbend.tcl
@@ -34,7 +34,7 @@ if {[file exists $scriptdir/ns2ir]} {
 
 set nfree "$updir/db/nfree"
 set libir "$scriptdir/ir/libir.tcl"
-set resetvlans "$scriptdir/resetvlans.tcl"
+set resetvlans "$scriptdir/resetvlans.pl"
 
 load $updir/sql.so
 
