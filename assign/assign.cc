8c5e28670c769f53f6be56e946491386b96444ab
Christopher Alfeld <calfeld@flux.utah.edu>
2000-12-26 22:26:57 +0000

    Improved the OPTIMAL_SCORE routine.  It wasn't taking into account all the direct_link scores that pop up.

--- assign/assign.cc
+++ assign/assign.cc
@@ -21,7 +21,8 @@
 // Purely heuristic
 #define OPTIMAL_SCORE(edges,nodes) (nodes*SCORE_PNODE + \
                                     nodes/5.0*SCORE_SWITCH + \
-                                    edges*(SCORE_INTRASWITCH_LINK*4+\
+                                    edges*((SCORE_INTRASWITCH_LINK+ \
+                                    SCORE_DIRECT_LINK*2)*4+\
                                            SCORE_INTERSWITCH_LINK)/5.0)
 
 tb_pgraph PG(1,1);
