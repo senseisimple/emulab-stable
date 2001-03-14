6e57f371b5acbe4c0c4cde04668ef3f2720af876
Christopher Alfeld <calfeld@flux.utah.edu>
2001-03-14 21:07:46 +0000

    Update for new multiple links commands and syntax.

--- www/tutorial/tb_compat.tcl
+++ www/tutorial/tb_compat.tcl
@@ -3,10 +3,12 @@
 
 proc tb-set-ip {node ip} {}
 proc tb-set-ip-interface {src dst ip} {}
+proc tb-set-ip-link {src link ip} {}
+proc tb-set-ip-lan {src lan ip} {}
 proc tb-set-hardware {node type args} {}
 proc tb-set-node-os {node os} {}
 proc tb-create-os {label path partition} {}
-proc tb-set-link-loss {src dst rate} {}
+proc tb-set-link-loss {src args} {}
 proc tb-set-lan-loss {lan rate} {}
 
 # The following commands are not clearly defined and probably will be
