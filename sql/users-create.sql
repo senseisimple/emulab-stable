15cb91f1f5c99b7792716c86dbdd42d698d2ced4
Robert Ricci <ricci@cs.utah.edu>
2001-06-05 23:13:12 +0000

    SQL commands for creating 'users' necessary to connect to the database

---
+++ sql/users-create.sql
@@ -0,0 +1,9 @@
+# MySQL dump 8.13
+#
+# Host: localhost    Database: mysql
+#--------------------------------------------------------
+# Server version	3.23.36-log
+
+INSERT INTO user VALUES ('paper','root','','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y');
+INSERT INTO user VALUES ('localhost','','','N','N','N','N','N','N','N','N','N','N','N','N','N','N');
+INSERT INTO user VALUES ('paper','','','N','N','N','N','N','N','N','N','N','N','N','N','N','N');
