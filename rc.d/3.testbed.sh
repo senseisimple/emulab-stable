8f582a64b4ed1265401472ba4c8c5428c16c894d
Robert Ricci <ricci@cs.utah.edu>
2001-06-06 00:13:02 +0000

    RC script for starting up local testbed services: bootinfo tmcd proxydhcp capture

---
+++ rc.d/3.testbed.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Utah Network Testbed local startup
+#
+if [ -x /usr/testbed/sbin/bootinfo.restart  ]; then
+        echo -n " bootinfo"
+        /usr/testbed/sbin/bootinfo.restart
+fi
+
+if [ -x /usr/testbed/sbin/tmcd.restart  ]; then
+        echo -n " tmcd"
+        /usr/testbed/sbin/tmcd.restart
+fi
+
+if [ -x /usr/testbed/sbin/proxydhcp.restart  ]; then
+	echo -n " proxydhcp"
+	/usr/testbed/sbin/proxydhcp.restart
+fi
+
+if [ -x /usr/site/etc/capture.rc -a -d /var/log/tiplogs ]; then
+	echo -n " capture"
+	/usr/site/etc/capture.rc
+fi
