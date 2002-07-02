d1611d97fa6fdd8e8a079eb08840db758174928a
Leigh B. Stoller <stoller@flux.utah.edu>
2002-07-02 16:16:44 +0000

    Check for error when doing a forced (from boss) update. If it does fail (usually cause of network dropout), continue to try every minute until it suceeds.

--- tmcd/ron/upemuman.sh
+++ tmcd/ron/upemuman.sh
@@ -6,8 +6,8 @@ chown root update vnodesetup
 chmod u+s update vnodesetup
 chown root /usr/bin/suidperl
 chmod u+s /usr/bin/suidperl
-chown emulabman client.pem emulab.pem
-chmod 640 client.pem emulab.pem
+chown emulabman client.pem emulab.pem ronnode.pem
+chmod 640 client.pem emulab.pem ronnode.pem
 /usr/bin/install -c -o root -g wheel -d -m 755 -o root -g 0 /var/testbed
 cp rc.testbed /usr/local/etc/rc.d/testbed.sh
 
