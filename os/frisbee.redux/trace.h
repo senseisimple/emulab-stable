2d9ee9c89c28102cbeb8798ceea702c57e09d96f
Mike Hibler <mike@flux.utah.edu>
2002-12-02 16:35:17 +0000

    copyrights

--- os/frisbee.redux/trace.h
+++ os/frisbee.redux/trace.h
@@ -1,3 +1,9 @@
+/*
+ * EMULAB-COPYRIGHT
+ * Copyright (c) 2002 University of Utah and the Flux Group.
+ * All rights reserved.
+ */
+
 #include <pthread.h>
 #include <sys/time.h>
 #include <netinet/in.h>
