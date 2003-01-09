1d19363ae7be441e6a075846dbf5071b578b2195
Mike Hibler <mike@flux.utah.edu>
2003-01-09 06:15:07 +0000

    Tune burstsize down some more. One I actually did the math (duh) 64 or even 48 is way too high. 16 works much better for the static case.

--- os/frisbee.redux/decls.h
+++ os/frisbee.redux/decls.h
@@ -90,7 +90,7 @@
  * clients' ability to generate requests which is limited by their ability
  * to decompress and write to disk.
  */
-#define SERVER_BURST_SIZE	48
+#define SERVER_BURST_SIZE	16
 #define SERVER_BURST_GAP	1000
 
 /*
