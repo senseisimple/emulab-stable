cbef12694a37a64ed72a5608b39abcfdfe0db4c7
Robert Ricci <ricci@cs.utah.edu>
2003-03-04 01:20:16 +0000

    (Optimisically) add the cat2980 to the list of supported hardware

--- tbsetup/snmpit_cisco_stack.pm
+++ tbsetup/snmpit_cisco_stack.pm
@@ -99,7 +99,7 @@ sub new($$$#@) {
 	# we weren't given devicenames for devices that aren't ciscos
 	#
 	SWITCH: for ($type) {
-	    (/cisco6509/ || /cisco4006/) && do {
+	    (/cisco6509/ || /cisco4006/ || /cisco2980/) && do {
 		use snmpit_cisco;
 		$device = new snmpit_cisco($devicename,$self->{DEBUG},$type,
 			$community,$supports_private);
