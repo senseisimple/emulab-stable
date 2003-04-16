85858667506ad855427e0488a5de5088eb26c9b3
Chad Barb <barb@flux.utah.edu>
2003-04-16 20:11:03 +0000

    Script and datafile to ensure consistency of off-site sitevariable definitions-- A lot like schemadiff.
    database-fill was inappropriate, since you don't want sitevars
    from the main site to overwrite remote sitevars.
    
    Needs to be integrated into install process, probably in the
    same way as schemadiff.

---
+++ sql/sitevars-create.sql
@@ -0,0 +1,17 @@
+-- MySQL dump 8.22
+--
+-- Host: localhost    Database: tbdb
+---------------------------------------------------------
+-- Server version	3.23.54-log
+
+--
+-- Dumping data for table 'sitevariables'
+--
+
+
+INSERT INTO sitevariables VALUES ('general/testvar',NULL,'43','A test variable');
+INSERT INTO sitevariables VALUES ('web/nologins',NULL,'0','Non-zero value indicates that no non-admin user may log into the Web Interface.');
+INSERT INTO sitevariables VALUES ('web/message',NULL,'','Message to place in large lettering under the login message on the Web Interface.');
+INSERT INTO sitevariables VALUES ('idle/threshold',NULL,'4','Number of hours of inactivity for a node/expt to be considered idle.');
+INSERT INTO sitevariables VALUES ('idle/mailinterval',NULL,'4','Number of hours since sending a swap request before sending another one. (Timing of first one is determined by idle/threshold.)');
+
