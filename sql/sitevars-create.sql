aeba52a13ebe85da18f108502d7074df7dd79c18
Mac Newbold <newbold@flux.utah.edu>
2003-04-21 18:01:46 +0000

    Add idle/cc_grp_ldrs sitevar, for when we should start cc'ing leaders on idle msgs.

--- sql/sitevars-create.sql
+++ sql/sitevars-create.sql
@@ -14,4 +14,5 @@ INSERT INTO sitevariables VALUES ('web/nologins',NULL,'0','Non-zero value indica
 INSERT INTO sitevariables VALUES ('web/message',NULL,'','Message to place in large lettering under the login message on the Web Interface.');
 INSERT INTO sitevariables VALUES ('idle/threshold',NULL,'4','Number of hours of inactivity for a node/expt to be considered idle.');
 INSERT INTO sitevariables VALUES ('idle/mailinterval',NULL,'4','Number of hours since sending a swap request before sending another one. (Timing of first one is determined by idle/threshold.)');
+INSERT INTO sitevariables VALUES ('idle/cc_grp_ldrs',NULL,'3','Start CC\'ing group and project leaders on idle messages on the Nth message.');
 
