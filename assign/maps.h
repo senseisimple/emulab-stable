fa420f2a261b74fd0fa70589c56ed343c52bdd9a
Robert Ricci <ricci@cs.utah.edu>
2003-06-23 16:49:09 +0000

    Add a header file that I had forgotten to check in.

---
+++ assign/maps.h
@@ -0,0 +1,33 @@
+/*
+ * EMULAB-COPYRIGHT
+ * Copyright (c) 2003 University of Utah and the Flux Group.
+ * All rights reserved.
+ */
+
+/*
+ * A simple header that provides definitions of some maps used in assign
+ */
+
+#ifndef __MAPS_H
+#define __MAPS_H
+
+/*
+ * A hash function for graph edges
+ */
+struct hashedge {
+  size_t operator()(vedge const &A) const {
+    hashptr<void *> ptrhash;
+    return ptrhash(target(A,VG))/2+ptrhash(source(A,VG))/2;
+  }
+};
+
+/*
+ * Map types
+ */
+typedef hash_map<vvertex,pvertex,hashptr<void *> > node_map;
+typedef hash_map<vvertex,bool,hashptr<void *> > assigned_map;
+typedef hash_map<pvertex,crope,hashptr<void *> > type_map;
+typedef hash_map<vedge,tb_link_info,hashedge> link_map;
+
+
+#endif
