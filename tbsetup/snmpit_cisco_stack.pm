577f3f8f084226ef6ba2ed0c626bd6424e240f03
Robert Ricci <ricci@cs.utah.edu>
2003-08-05 00:14:43 +0000

    Add the cat2950 as a supported switch type.

--- tbsetup/snmpit_cisco_stack.pm
+++ tbsetup/snmpit_cisco_stack.pm
@@ -99,7 +99,8 @@ sub new($$$#@) {
 	# we weren't given devicenames for devices that aren't ciscos
 	#
 	SWITCH: for ($type) {
-	    (/cisco6509/ || /cisco4006/ || /cisco2980/) && do {
+	    (/cisco6509/ || /cisco4006/ || /cisco2980/ || /catalyst2950/)
+		    && do {
 		use snmpit_cisco;
 		$device = new snmpit_cisco($devicename,$self->{DEBUG},$type,
 			$community,$supports_private);
