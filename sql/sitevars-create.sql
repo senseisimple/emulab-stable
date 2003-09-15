8917c468518d76d4b40f22f1a350c9b45c3b0cef
Leigh B. Stoller <stoller@flux.utah.edu>
2003-09-15 16:18:57 +0000

    Add general/recently_active sitevar. Number of days since last experiment action.

--- sql/sitevars-create.sql
+++ sql/sitevars-create.sql
@@ -11,12 +11,13 @@
 
 INSERT INTO sitevariables VALUES ('general/testvar',NULL,'43','A test variable');
 INSERT INTO sitevariables VALUES ('web/nologins',NULL,'0','Non-zero value indicates that no non-admin user may log into the Web Interface.');
-INSERT INTO sitevariables VALUES ('web/message',NULL,'','Message to place in large lettering under the login message on the Web Interface.');
-INSERT INTO sitevariables VALUES ('idle/threshold',NULL,'4','Number of hours of inactivity for a node/expt to be considered idle.');
+INSERT INTO sitevariables VALUES ('web/message','Cluster use limited to those with imminent deadlines!','','Message to place in large lettering under the login message on the Web Interface.');
+INSERT INTO sitevariables VALUES ('idle/threshold','1','4','Number of hours of inactivity for a node/expt to be considered idle.');
 INSERT INTO sitevariables VALUES ('idle/mailinterval',NULL,'4','Number of hours since sending a swap request before sending another one. (Timing of first one is determined by idle/threshold.)');
 INSERT INTO sitevariables VALUES ('idle/cc_grp_ldrs',NULL,'3','Start CC\'ing group and project leaders on idle messages on the Nth message.');
 INSERT INTO sitevariables VALUES ('batch/retry_wait',NULL,'900','Number of seconds to wait before retrying a failed batch experiment.');
 INSERT INTO sitevariables VALUES ('swap/idleswap_warn',NULL,'30','Number of minutes before an Idle-Swap to send a warning message. Set to 0 for no warning.');
 INSERT INTO sitevariables VALUES ('swap/autoswap_warn',NULL,'60','Number of minutes before an Auto-Swap to send a warning message. Set to 0 for no warning.');
 INSERT INTO sitevariables VALUES ('idle/batch_threshold',NULL,'30','Number of minutes of inactivity for a batch node/expt to be considered idle.');
+INSERT INTO sitevariables VALUES ('general/recently_active',NULL,'7','Number of days to be considered a recently active user of the testbed.');
 
