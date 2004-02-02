f67ad04bfb228839595992e7b48ebe0b14d6ef82
Jonathon Lynn Duerig <duerig@flux.utah.edu>
2004-02-02 20:27:20 +0000

    Fixed compile script/instructions

--- tbsetup/ipassign/compile.sh
+++ tbsetup/ipassign/compile.sh
@@ -1,33 +1,26 @@
 #!/bin/sh
 
-export EXTRA_LIB_PATH=/users/duerig
+export EXTRA_LIB_PATH=/home/duerig/metis/metis-4.0
+export EXTRA_INCLUDE_PATH=/home/duerig/metis/metis-4.0/Lib/
+g++ -O3 -c -o tmp/ipassign.o -I${EXTRA_INCLUDE_PATH} src/ipassign.cc
+g++ -O3 -c -o tmp/bitmath.o -I${EXTRA_INCLUDE_PATH} src/bitmath.cc
+g++ -O3 -c -o tmp/Assigner.o -I${EXTRA_INCLUDE_PATH} src/Assigner.cc
+g++ -O3 -c -o tmp/ConservativeAssigner.o -I${EXTRA_INCLUDE_PATH} src/ConservativeAssigner.cc
+g++ -O3 -c -o tmp/Framework.o -I${EXTRA_INCLUDE_PATH} src/Framework.cc
+g++ -O3 -c -o tmp/HostRouter.o -I${EXTRA_INCLUDE_PATH} src/HostRouter.cc
+g++ -O3 -c -o tmp/Router.o -I${EXTRA_INCLUDE_PATH} src/Router.cc
+g++ -O3 -c -o tmp/LanRouter.o -I${EXTRA_INCLUDE_PATH} src/LanRouter.cc
+g++ -O3 -c -o tmp/NetRouter.o -I${EXTRA_INCLUDE_PATH} src/NetRouter.cc
+g++ -O3 -c -o tmp/coprocess.o -I${EXTRA_INCLUDE_PATH} src/coprocess.cc
+g++ -O3 -o bin/ipassign -L${EXTRA_LIB_PATH}/ tmp/ipassign.o tmp/bitmath.o tmp/Assigner.o tmp/ConservativeAssigner.o tmp/Framework.o tmp/HostRouter.o tmp/Router.o tmp/LanRouter.o tmp/NetRouter.o tmp/coprocess.o -lmetis -lm
 
-#g++ -O3 -c -o tmp/ipassign.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/ipassign.cc
-#g++ -O3 -c -o tmp/bitmath.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/bitmath.cc
-#g++ -O3 -c -o tmp/Assigner.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/Assigner.cc
-#g++ -O3 -c -o tmp/ConservativeAssigner.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/ConservativeAssigner.cc
-#g++ -O3 -c -o tmp/Framework.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/Framework.cc
-#g++ -O3 -c -o tmp/HostRouter.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/HostRouter.cc
-#g++ -O3 -c -o tmp/Router.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/Router.cc
-#g++ -O3 -c -o tmp/LanRouter.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/LanRouter.cc
-#g++ -O3 -c -o tmp/NetRouter.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/NetRouter.cc
-g++ -O3 -c -o tmp/coprocess.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/coprocess.cc
-g++ -O3 -o bin/ipassign -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ -L${EXTRA_LIB_PATH}/metis/metis-4.0 tmp/ipassign.o tmp/bitmath.o tmp/Assigner.o tmp/ConservativeAssigner.o tmp/Framework.o tmp/HostRouter.o tmp/Router.o tmp/LanRouter.o tmp/NetRouter.o tmp/coprocess.o -lmetis -lm
+g++ -O3 -c -o tmp/routestat.o -I${EXTRA_INCLUDE_PATH} src/routestat.cc
+g++ -O3 -o bin/routestat -L${EXTRA_LIB_PATH} tmp/routestat.o tmp/bitmath.o -lmetis -lm
 
-g++ -O3 -c -o tmp/routestat.o -I${EXTRA_LIB_PATH}/metis/metis-4.0/Lib/ src/routestat.cc
-g++ -O3 -o bin/routestat -L${EXTRA_LIB_PATH}/metis/metis-4.0 tmp/routestat.o tmp/bitmath.o -lmetis -lm
+g++ -O3 -o bin/routecalc src/routecalc.cc
 
-#g++ -O3 -o bin/routecalc src/routecalc.cc
+g++ -o bin/inet2graph src/inet2graph.cc -lm
+g++ -o bin/brite2graph src/brite2graph.cc -lm
 
-#g++ -o bin/inet2graph src/inet2graph.cc -lm
-#g++ -o bin/brite2graph src/brite2graph.cc -lm
+g++ -o bin/top2graph src/top2graph.cc -lm
 
-#g++ -o bin/top2graph src/top2graph.cc -lm
-
-#g++ -o bin/autocheck src/autocheck.cc -lm
-#g++ -o bin/boolcmp src/boolcmp.cc -lm
-
-#bin/autocheck
-
-
-#27338.150u 429.446s 12:16:11.56 62.8%   430+-267k 1+2584io 1998215pf+0w
