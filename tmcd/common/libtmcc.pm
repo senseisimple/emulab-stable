4d037e7c05f533aca22dacf106495e73dbda6245
Mike Hibler <mike@flux.utah.edu>
2004-03-19 06:03:19 +0000

    The new watchdog: 	- check intervals driven by sitevars delivered by TMCD command 	- handles rusage stats return on plab nodes It is now a single process and executes any auxilliary scripts synchronously. This may prove to be unwieldy in the face of long running scripts like update.  If so, we'll have to add all that fork/exec/waitpid mucky-muck.

--- tmcd/common/libtmcc.pm
+++ tmcd/common/libtmcc.pm
@@ -26,7 +26,7 @@ use Exporter;
 	     TMCCCMD_PLABCONFIG TMCCCMD_SUBCONFIG TMCCCMD_LINKDELAYS
 	     TMCCCMD_PROGRAMS TMCCCMD_SYNCSERVER TMCCCMD_KEYHASH TMCCCMD_NODEID
 	     TMCCCMD_NTPINFO TMCCCMD_NTPDRIFT TMCCCMD_EVENTKEY TMCCCMD_ROUTELIST
-	     TMCCCMD_ROLE TMCCCMD_RUSAGE
+	     TMCCCMD_ROLE TMCCCMD_RUSAGE TMCCCMD_WATCHDOGINFO
 	     );
 
 # Must come after package declaration!
@@ -147,6 +147,7 @@ my %commandset =
       "routelist"	=> {TAG => "routelist"},
       "role"		=> {TAG => "role"},
       "rusage"		=> {TAG => "rusage"},
+      "watchdoginfo"	=> {TAG => "watchdoginfo"},
     );
 
 #
@@ -188,6 +189,7 @@ sub TMCCCMD_EVENTKEY()  { $commandset{"eventkey"}->{TAG}; }
 sub TMCCCMD_ROUTELIST()	{ $commandset{"routelist"}->{TAG}; }
 sub TMCCCMD_ROLE()	{ $commandset{"role"}->{TAG}; }
 sub TMCCCMD_RUSAGE()	{ $commandset{"rusage"}->{TAG}; }
+sub TMCCCMD_WATCHDOGINFO(){ $commandset{"watchdoginfo"}->{TAG}; }
 
 #
 # Caller uses this routine to set configuration of this library
