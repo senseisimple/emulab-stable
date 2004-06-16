2202a05933d12ce381e837093dceca5d20923bf9
Shashi Guruprasad <shash@flux.utah.edu>
2004-06-16 21:38:20 +0000

    A fix to correctly update the virt_agents table. Some hacks to allow the NSE scheduler slop factor to be controlled from the user-specified make-simulated code. Also added a null tb_compat file so we can ignore any tb-* commands that get specified inside make-simulated. These commands are anyway evaluated during the initial parse.

---
+++ tbsetup/nseparse/tb_compat.tcl
@@ -0,0 +1,75 @@
+# -*- tcl -*-
+#
+# EMULAB-COPYRIGHT
+# Copyright (c) 2000-2004 University of Utah and the Flux Group.
+# All rights reserved.
+#
+
+# This is a nop tb_compact.tcl file that should be used when running scripts
+# under ns.
+
+proc tb-set-ip {node ip} {}
+proc tb-set-ip-interface {src dst ip} {}
+proc tb-set-ip-link {src link ip} {}
+proc tb-set-ip-lan {src lan ip} {}
+proc tb-set-hardware {node type args} {}
+proc tb-set-node-os {node os} {}
+proc tb-set-link-loss {src args} {}
+proc tb-set-lan-loss {lan rate} {}
+proc tb-set-node-rpms {node args} {}
+proc tb-set-node-startup {node cmd} {}
+proc tb-set-node-cmdline {node cmd} {}
+proc tb-set-node-tarfiles {node args} {}
+proc tb-set-node-lan-delay {node lan delay} {}
+proc tb-set-node-lan-bandwidth {node lan bw} {}
+proc tb-set-node-lan-loss {node lan loss} {}
+proc tb-set-node-lan-params {node lan delay bw loss} {}
+proc tb-set-node-failure-action {node type} {}
+proc tb-set-ip-routing {type} {}
+proc tb-fix-node {v p} {}
+proc tb-make-weighted-vtype {name weight types} {}
+proc tb-make-soft-vtype {name types} {}
+proc tb-make-hard-vtype {name types} {}
+proc tb-set-lan-simplex-params {lan node todelay tobw toloss fromdelay frombw fromloss} {}
+proc tb-set-link-simplex-params {link src delay bw loss} {}
+proc tb-set-uselatestwadata {onoff} {}
+proc tb-set-usewatunnels {onoff} {}
+proc tb-set-wasolver-weights {delay bw plr} {}
+proc tb-use-endnodeshaping {onoff} {}
+proc tb-force-endnodeshaping {onoff} {}
+proc tb-set-multiplexed {link onoff} {}
+proc tb-set-endnodeshaping {link onoff} {}
+proc tb-set-noshaping {link onoff} {}
+proc tb-set-useveth {link onoff} {}
+proc tb-set-allowcolocate {lanlink onoff} {}
+proc tb-set-colocate-factor {factor} {}
+proc tb-set-node-startcmd {node cmd} {}
+proc tb-set-encapsulate {onoff} {}
+proc tb-set-jail-os {os} {}
+proc tb-set-delay-os {os} {}
+proc tb-use-ipassign {onoff} {}
+proc tb-set-ipassign-args {args} {}
+proc tb-set-lan-protocol {lanlink protocol} {}
+proc tb-set-lan-accesspoint {lanlink node} {}
+proc tb-set-lan-setting {lanlink capkey capval} {}
+proc tb-set-node-lan-setting {lanlink node capkey capval} {}
+
+Class Program
+
+Program instproc init {args} {
+}
+
+Program instproc unknown {m args} {
+}
+
+# We are just syntax checking the NS file
+Simulator instproc run {args} {
+}
+
+Simulator instproc nsenode {args} {
+    return [new NSENode]
+}
+
+Simulator instproc make-simulated {args} {
+    uplevel 1 eval $args
+}
