b160df1a6376e8d4211995e81cf473a2b6593e91
Russ Fish <fish@flux.utah.edu>
2004-06-17 17:55:10 +0000

    Add Emulab copyright notices.

--- hyperviewer/hypview.py
+++ hyperviewer/hypview.py
@@ -2,6 +2,20 @@
 #
 # hypview - HyperViewer application.
 # For description of script args, invoke with any dash arg or see the "usage:" message below.
+#
+# EMULAB-COPYRIGHT
+# Copyright (c) 2004 University of Utah and the Flux Group.
+# All rights reserved.
+#
+# Permission to use, copy, modify and distribute this software is hereby
+# granted provided that (1) source code retains these copyright, permission,
+# and disclaimer notices, and (2) redistributions including binaries
+# reproduce the notices in supporting documentation.
+#
+# THE UNIVERSITY OF UTAH ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
+# CONDITION.  THE UNIVERSITY OF UTAH DISCLAIMS ANY LIABILITY OF ANY KIND
+# FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+#
 
 ##import pdb
 
