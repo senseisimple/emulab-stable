de3456e2545a5680d555b74ead8c7457a9e1693e
Mike Hibler <mike@flux.utah.edu>
2004-06-17 20:46:56 +0000

    Unexpand copyrights

--- hyperviewer/hvmain.cpp
+++ hyperviewer/hvmain.cpp
@@ -1,18 +1,11 @@
-// hvmain.cpp - hypview for SWIG into _hv.so, based on hypviewer/examples/glut/main.cpp .
 //
 // EMULAB-COPYRIGHT
 // Copyright (c) 2004 University of Utah and the Flux Group.
 // All rights reserved.
 //
-// Permission to use, copy, modify and distribute this software is hereby
-// granted provided that (1) source code retains these copyright, permission,
-// and disclaimer notices, and (2) redistributions including binaries
-// reproduce the notices in supporting documentation.
-//
-// THE UNIVERSITY OF UTAH ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
-// CONDITION.  THE UNIVERSITY OF UTAH DISCLAIMS ANY LIABILITY OF ANY KIND
-// FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
-//
+
+// hvmain.cpp - hypview for SWIG into _hv.so, based on hypviewer/examples/glut/main.cpp .
+
 #include <string>
 NAMESPACEHACK
 
