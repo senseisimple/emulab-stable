d861a0a77346a83b9477c996ac53e7dec42bd4bb
Jonathon Lynn Duerig <duerig@flux.utah.edu>
2004-07-31 04:08:06 +0000

    Added three helper programs. These are utilities to make it easier to directly interface the output of ipassign to the input of distributed-dijkstra. Using these utilities, they can be tested as a unit without having to go through the rest of the infrastructure of emulab.

---
+++ tbsetup/ipassign/src/graph2single-source.cc
@@ -0,0 +1,45 @@
+// graph2single-source.cc
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int bits = 0;
+    int weight = 0;
+    vector<int> hosts;
+    string lineString;
+    getline(cin, lineString);
+    while (cin)
+    {
+        hosts.clear();
+        istringstream line(lineString);
+        int tempHost = 0;
+
+        line >> bits >> weight;
+        line >> tempHost;
+        while(line)
+        {
+            hosts.push_back(tempHost);
+            line >> tempHost;
+        }
+
+        for (size_t i = 0; i < hosts.size(); ++i)
+        {
+            for (size_t j = i + 1; j < hosts.size(); ++j)
+            {
+                cout << "I " << hosts[i] << " " << hosts[j] << " "
+                     << static_cast<float>(weight) << endl;
+            }
+        }
+
+        getline(cin, lineString);
+    }
+
+    cout << "C" << endl;
+    return 0;
+}
---
+++ tbsetup/ipassign/src/route2dist.cc
@@ -0,0 +1,93 @@
+// route2dist.cc
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+struct Lan
+{
+    Lan() : number(0) {}
+    int number;
+    vector<int> hosts;
+    vector<string> ips;
+};
+
+int maxHost = 0;
+
+void addToLan(Lan & theLan, string & streamString)
+{
+    istringstream stream(streamString);
+    int temp;
+    stream >> temp;
+    int hostNumber = 0;
+    string ip;
+    stream >> hostNumber;
+    stream >> ip;
+
+    maxHost = max(maxHost, hostNumber);
+
+    theLan.hosts.push_back(hostNumber);
+    theLan.ips.push_back(ip);
+}
+
+int main()
+{
+    list<Lan> lanList;
+    string cache;
+    getline(cin, cache);
+    int lanNumber = 0;
+    stringstream cacheStream;
+    if (cin)
+    {
+        cacheStream << cache;
+        cacheStream >> lanNumber;
+    }
+    while (cin)
+    {
+        lanList.push_back(Lan());
+        lanList.back().number = lanNumber;
+//        addToLan(lanList.back(), cacheStream);
+
+//        getline(cin, cache);
+        while (cin && lanNumber == lanList.back().number)
+        {
+            addToLan(lanList.back(), cache);
+            getline(cin, cache);
+            if (cin)
+            {
+                istringstream stream(cache);
+                stream >> lanNumber;
+            }
+        }
+    }
+    cerr << lanList.size() << endl;
+    int lanCount = 0;
+    list<Lan>::iterator countPos = lanList.begin();
+    list<Lan>::iterator countLimit = lanList.end();
+    for ( ; countPos != countLimit; ++countPos)
+    {
+        lanCount += (static_cast<int>(countPos->hosts.size())
+                     * (static_cast<int>(countPos->hosts.size()) - 1)) >> 1;
+    }
+    cout << maxHost + 1 << " " << lanCount << endl;
+
+    list<Lan>::iterator pos = lanList.begin();
+    list<Lan>::iterator limit = lanList.end();
+    for ( ; pos != limit; ++pos)
+    {
+        for (size_t i = 0; i < pos->hosts.size(); ++i)
+        {
+            for (size_t j = i + 1; j < pos->hosts.size(); ++j)
+            {
+                cout << pos->hosts[i] << " " << pos->ips[i] << " ";
+                cout << pos->hosts[j] << " " << pos->ips[j] << " 1" << endl;
+            }
+        }
+    }
+    return 0;
+}
+
