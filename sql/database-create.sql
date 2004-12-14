8a5d1f1e2b751fb6c0a916568423774873f94c45
Russ Fish <fish@flux.utah.edu>
2004-12-14 04:17:49 +0000

    Add Windows to the OS list in the os_info table.

--- sql/database-create.sql
+++ sql/database-create.sql
@@ -1156,7 +1156,7 @@ CREATE TABLE os_info (
   creator varchar(8) default NULL,
   created datetime default NULL,
   description tinytext NOT NULL,
-  OS enum('Unknown','Linux','FreeBSD','NetBSD','OSKit','Other') NOT NULL default 'Unknown',
+  OS enum('Unknown','Linux','FreeBSD','NetBSD','OSKit','Windows','Other') NOT NULL default 'Unknown',
   version varchar(12) default '',
   path tinytext,
   magic tinytext,
