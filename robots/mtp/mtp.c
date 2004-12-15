c961a0a1407a172a3026388ddf4533ff4166e231
Timothy Stack <stack@flux.utah.edu>
2004-12-15 07:11:31 +0000

    include some headers

--- robots/mtp/mtp.c
+++ robots/mtp/mtp.c
@@ -1,6 +1,7 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <inttypes.h>
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
--- robots/mtp/mtp_recv.c
+++ robots/mtp/mtp_recv.c
@@ -2,6 +2,7 @@
 #include "config.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
