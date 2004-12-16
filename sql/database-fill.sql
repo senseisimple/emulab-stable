5f7792e33a98df817cc89b43a61af41cfa5102b9
Timothy Stack <stack@flux.utah.edu>
2004-12-16 17:40:46 +0000

    Add timeline and sequence event_objecttypes

--- sql/database-fill.sql
+++ sql/database-fill.sql
@@ -98,6 +98,8 @@ REPLACE INTO event_objecttypes VALUES (7,'LINKTEST');
 REPLACE INTO event_objecttypes VALUES (8,'NSE');
 REPLACE INTO event_objecttypes VALUES (9,'SLOTHD');
 REPLACE INTO event_objecttypes VALUES (10,'NODE');
+REPLACE INTO event_objecttypes VALUES (11,'SEQUENCE');
+REPLACE INTO event_objecttypes VALUES (12,'TIMELINE');
 
 --
 -- Dumping data for table `exported_tables`
@@ -612,7 +614,6 @@ REPLACE INTO table_regex VALUES ('experiments','jail_osname','text','redirect','
 REPLACE INTO table_regex VALUES ('experiments','delay_osname','text','redirect','os_info:osname',0,0,NULL);
 REPLACE INTO table_regex VALUES ('experiments','use_ipassign','int','redirect','default:boolean',0,0,NULL);
 REPLACE INTO table_regex VALUES ('experiments','ipassign_args','text','regex','^[\\w\\s-]*$',0,255,NULL);
-REPLACE INTO table_regex VALUES ('experiments','security_level','int','redirect','default:tinyuint',0,4,NULL);
 REPLACE INTO table_regex VALUES ('nodes','bios_version','text','regex','^[-\\w\\.+]+$',0,64,NULL);
 REPLACE INTO table_regex VALUES ('os_info','osid','text','regex','^[-\\w\\.+]+$',2,35,NULL);
 REPLACE INTO table_regex VALUES ('experiments','expt_name','text','redirect','default:tinytext',1,255,NULL);
@@ -669,9 +670,10 @@ REPLACE INTO table_regex VALUES ('firewall_rules','ruleno','int','redirect','def
 REPLACE INTO table_regex VALUES ('firewall_rules','rule','text','regex','^\\w[-\\w \\t,/\\{\\}\\(\\)!:\\.]*$',0,1024,NULL);
 REPLACE INTO table_regex VALUES ('virt_nodes','inner_elab_role','text','regex','^(boss|ops|node)$',0,0,NULL);
 REPLACE INTO table_regex VALUES ('experiments','elab_in_elab','int','redirect','default:boolean',0,0,NULL);
-REPLACE INTO table_regex VALUES ('experiments','elabinelab_eid','text','redirect','experiments:eid',0,0,NULL);
 REPLACE INTO table_regex VALUES ('images','imageid','text','regex','^[a-zA-Z0-9][-\\w\\.+]+$',0,45,NULL);
 REPLACE INTO table_regex VALUES ('images','imagename','text','regex','^[a-zA-Z0-9][-\\w\\.+]+$',2,30,NULL);
+REPLACE INTO table_regex VALUES ('experiments','security_level','int','redirect','default:tinyuint',0,4,NULL);
+REPLACE INTO table_regex VALUES ('experiments','elabinelab_eid','text','redirect','experiments:eid',0,0,NULL);
 
 --
 -- Dumping data for table `testsuite_preentables`
