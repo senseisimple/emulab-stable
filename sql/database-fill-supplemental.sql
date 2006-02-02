11f6065f8a6506d1227c0e4a0f7cac59ba911c06
Timothy Stack <stack@flux.utah.edu>
2006-02-02 15:06:52 +0000

    Finish off changes to consult the os_boot_cmd table when setting def_boot_cmd_line.
    	* db/libdb.pm.in: Add TBGetOSBootCmd function that returns the
    	boot command line for an osid/role.
    
    	* sql/database-fill-supplemental.sql: Add some queries that
    	initialize the os_boot_cmd table.
    
    	* tbsetup/assign_wrapper.in: For nodes that need linkdelays or
    	inner elab boss nodes, set their command line using
    	TBGetOSBootCmd.  Since the TBGetOSBootCmd function needs a "real"
    	OSID to work, we resolve the nextosid for any meta OSIDs.
    
    	* tbsetup/elabinelab.in: Add os_boot_cmd to the list of full
    	tables to be dumped.
    
    	* tbsetup/ns2ir/elabinelab-withfsnode.ns,
    	tbsetup/ns2ir/elabinelab.ns: Remove boot command lines,
    	assign_wrapper does it instead.

--- sql/database-fill-supplemental.sql
+++ sql/database-fill-supplemental.sql
@@ -1,5 +1,5 @@
 -- 
--- database-create-supplemental.sql - Various things that need to go into new
+-- database-fill-supplemental.sql - Various things that need to go into new
 -- sites' databases, but don't really fit into database-fill.sql, which is
 -- auto-generated. Also, unlike the contents of database-fill.sql, inserting
 -- these is not idempotent, since a site may have changed them for some reason.
@@ -11,3 +11,10 @@ INSERT IGNORE INTO os_info VALUES ('NEWNODE-MFS','emulab-ops','NEWNODE-MFS','roo
 INSERT IGNORE INTO os_info VALUES ('OPSNODE-BSD','emulab-ops','OPSNODE-BSD','root',NULL,'FreeBSD on the Operations Node','FreeBSD','4.X','',NULL,'','ping,ssh,ipod,isup',0,1,0,'OPSNODEBSD',NULL,NULL,1,150);
 INSERT IGNORE INTO os_info VALUES ('FW-IPFW','emulab-ops','FW-IPFW','root',NULL,'IPFW Firewall','FreeBSD','',NULL,'FreeBSD','','ping,ssh,ipod,isup,veths,mlinks',0,1,1,'NORMAL','emulab-ops-FBSD47-STD',NULL,0,150);
 INSERT IGNORE INTO node_types VALUES ('pcvm','pcvm','PIII',0,0,0.00,0,'emulab-ops-FBSD-JAIL',0,60,'',0,0,0,'eth0',NULL,0,'','',NULL,NULL,'',1,0,1,1,0,0,0,0,0,0,0,1,NULL,'FREEBSD-MFS','FRISBEE-MFS');
+
+INSERT IGNORE INTO os_boot_cmd VALUES ('FreeBSD','4.10','delay','/kernel.delay');
+INSERT IGNORE INTO os_boot_cmd VALUES ('FreeBSD','4.10','vnodehost','/kernel.jail');
+INSERT IGNORE INTO os_boot_cmd VALUES ('FreeBSD','4.10','linkdelay','/kernel.linkdelay');
+INSERT IGNORE INTO os_boot_cmd VALUES ('FreeBSD','5.4','delay','/boot/kernel.delay/kernel');
+INSERT IGNORE INTO os_boot_cmd VALUES ('FreeBSD','5.4','linkdelay','/boot/kernel.linkdelay/kernel');
+INSERT IGNORE INTO os_boot_cmd VALUES ('Linux','9.0','linkdelay','linkdelay');
