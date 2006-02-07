fa3b52870c1245e5e375ed81c0ed330e3be99b22
Kevin Atkinson <kevina@cs.utah.edu>
2006-02-07 00:11:38 +0000

    Added a reboot test on the single node experments

--- image-test/tests/single/tests.pl
+++ image-test/tests/single/tests.pl
@@ -5,3 +5,5 @@ test_rcmd 'tgz', ['tar'], 'node', 'test -e /usr/tgz/hw.txt';
 test_rcmd 'tar.gz', ['tar'], 'node', 'test -e /usr/tar.gz/hw.txt';
 test_rcmd 'tar.bz2', ['tar'], 'node', 'test -e /usr/tar.bz2/hw.txt';
 test_rcmd 'startcmd', [], 'node', 'test -e /tmp/startcmd-ok';
+test_cmd 'reboot', [], "node_reboot -w -e $parms{pid},$parms{eid}";
+
