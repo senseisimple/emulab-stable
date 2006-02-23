ddbd1c92b6bcecfc2d5a8317ef63e54ec8ea3228
Leigh B. Stoller <stoller@flux.utah.edu>
2006-02-23 17:08:37 +0000

    Make our rc.d scripts play properly on FreeBSD6, which has a new wildly obfuscated rc.d mechanism.

--- rc.d/1.mysql-client.sh
+++ rc.d/1.mysql-client.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# PROVIDE: mysql-client
+# REQUIRE: NETWORKING SERVERS ldconfig
+# BEFORE: mysql apache
+# KEYWORD: shutdown
+
 case "$1" in
-	start)
+	start|faststart)
 		/sbin/ldconfig -m /usr/local/lib/mysql
 		;;
 	stop)
