639fc5c56316463ad56f61c13d3a666cdadc049f
Robert Ricci <ricci@cs.utah.edu>
2006-03-22 01:11:56 +0000

    Oops, off-by-one error in argv handling

--- pelab/iperfd/iperfd.c
+++ pelab/iperfd/iperfd.c
@@ -106,7 +106,7 @@ int main(int argc, char **argv) {
      */
     fprinttimestamp(stdout);
     printf(" iperfd starting, called as: ");
-    for (int i = 0; i<= argc; i++) {
+    for (int i = 0; i < argc; i++) {
         printf(argv[i]);
         if (i != argc) {
             printf(" ");
