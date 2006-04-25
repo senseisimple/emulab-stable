665a8e692f1f0b9d021fd0c13f7f4ca3078499f2
Mike Hibler <mike@flux.utah.edu>
2006-04-25 00:16:41 +0000

    New shi...um, "stuff" to allow running tcpdump on end nodes for tracing.
    Set the magic variable in auto-pelab.ns and swapin your experiment.
    Logs will wind up in /proj/blahblah/exp/blahblah/logs/<node>/<name>.tcpdump
    after you run stop-experiment.
    
    I did this with the program agent and duct tape.  If it turns out that
    you can already do this with the link tracing framework, then someone in
    Oregon is going to whomp me upside the head with a clue-stick and he'll
    never let me forget...at least til he forgets in a day or two.

---
+++ pelab/run-tcpdump.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]; then
+    echo "usage: run-tcpdump type index [ tcpdump-args ... ]"
+    exit 1
+fi
+
+kind=$1
+if [ $kind = "planet" ]; then
+    echo "cannot do real planetlab nodes yet"
+    exit 1
+fi
+
+ix=$2
+
+shift; shift
+ARGS=$*
+
+findif="/proj/tbres/bin/findif-linux"
+if [ ! -x $findif ]; then
+    echo "need $findif"
+    exit 1
+fi
+
+case $kind in
+elab)
+    ip="10.0.0.$ix"
+    iface=`$findif -i $ip`
+    ;;
+plab)
+    ip="10.1.0.$ix"
+    iface=`$findif -i $ip`
+    ;;
+*)
+    echo "unrecognized node type $kind"
+    exit 1
+    ;;
+esac
+
+if [ -z "$ip" ]; then
+    echo "Could not find interface for IP $ip"
+    exit 1
+fi
+
+ARGS="-i $iface -w /local/logs/$kind-$ix.tcpdump $ARGS"
+echo "$kind-${ix}: Running tcpdump $ARGS"
+sudo tcpdump $ARGS &
+PID=$!
+trap "sudo kill $PID; sudo killall tcpdump" EXIT
+wait
+exit 0
