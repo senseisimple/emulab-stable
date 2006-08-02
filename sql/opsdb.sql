8eda62e4e72f95ae0076c6b0510e026aac683464
Leigh B. Stoller <stoller@flux.utah.edu>
2006-08-02 19:51:05 +0000

    Minor changes to the opsdb schema; add a timestamp to record when the DB was created, and a flag to indicate if the DB is transient (and can be garbage collected).

--- sql/opsdb.sql
+++ sql/opsdb.sql
@@ -6,3 +6,7 @@ CREATE TABLE IF NOT EXISTS emulab_dbs (
   dbname varchar(64) NOT NULL default '',
   PRIMARY KEY  (dbname)
 ) TYPE=MyISAM;
+
+alter table emulab_dbs add date_created datetime default NULL;
+update emulab_dbs set date_created=now();
+alter table emulab_dbs add temporary tinyint(1) NOT NULL default '0';
