86c8a7629c1c3cc355e68553338ebee4952fe749
Robert Ricci <ricci@cs.utah.edu>
2006-10-02 21:51:51 +0000

    Fix a datatype issue - we were storing the results of a 'long long' function in an int.
    DelaySensor no longer uses retransmitted packets to calculate delays.
    This is mandated by RFC 2988.
    
    Add more debugging output when we detect a bogus (<0) delay.

--- pelab/magent/DelaySensor.cc
+++ pelab/magent/DelaySensor.cc
@@ -41,12 +41,13 @@ void DelaySensor::localAck(PacketInfo * packet)
 {
   sendValid = false;
   /*
-   * XXX: According to RFC 2988, TCP MUST use Karn's algorithm for RTT
+   * According to RFC 2988, TCP MUST use Karn's algorithm for RTT
    * calculation, which means that it cannot use retransmitted packets to
    * caculate RTT (since it is ambiguous which of the two packets is being
    * ACKed) unless using TCP timestamps
    */
   if (state->isAckValid() && packetHistory->isAckValid()
+      && !packetHistory->getIsRetransmit()
       && state->getState() == StateSensor::ESTABLISHED)
   {
     Time diff = packet->packetTime - packetHistory->getAckedSendTime();
@@ -54,7 +55,9 @@ void DelaySensor::localAck(PacketInfo * packet)
     // Failsafe - if this happens, we need to fix the cause, but in the
     // meantime, let's just make sure that it doesn't make it to the monitor
     if (lastDelay < 0) {
-      logWrite(ERROR, "DelaySensor::localAck() Bogus delay %d", lastDelay);
+      logWrite(ERROR, "DelaySensor::localAck() Bogus delay %lli (%lli - %lli)", lastDelay,
+              packet->packetTime.toMilliseconds(),
+              packetHistory->getAckedSendTime().toMilliseconds());
       ackValid = false;
     } else {
       logWrite(SENSOR, "DELAY: %d ms", lastDelay);
