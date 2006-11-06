02a4368849c24f401d12e0e05e3cfcf091065ce2
Leigh B. Stoller <stoller@flux.utah.edu>
2006-11-06 15:36:25 +0000

    Another class file ...

---
+++ www/experiment_defs.php
@@ -0,0 +1,13 @@
+<?php
+#
+# EMULAB-COPYRIGHT
+# Copyright (c) 2006 University of Utah and the Flux Group.
+# All rights reserved.
+#
+#
+# This class is really just a wrapper around the DB data ...
+#
+class Experiment
+{
+
+}
