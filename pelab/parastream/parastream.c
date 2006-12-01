ef666d828552910231c81e80fef10f62084a99f3
David Johnson <johnsond@flux.utah.edu>
2006-12-01 23:14:33 +0000

    Copyrights.

--- pelab/parastream/parastream.c
+++ pelab/parastream/parastream.c
@@ -1,3 +1,9 @@
+/*
+ * EMULAB-COPYRIGHT
+ * Copyright (c) 2006 University of Utah and the Flux Group.
+ * All rights reserved.
+ */
+
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
