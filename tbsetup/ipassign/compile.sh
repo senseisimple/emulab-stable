65ee4b3ea8ff74ac9989b70fb41b0ac9a6ac9a17
Jonathon Lynn Duerig <duerig@flux.utah.edu>
2006-12-01 18:06:30 +0000

    Removed dead code. Added a comment.

--- tbsetup/ipassign/compile.sh
+++ tbsetup/ipassign/compile.sh
@@ -8,12 +8,11 @@ export LINK_FLAGS=-g
 
 ${CC} ${COMPILE_FLAGS} -c -o tmp/ipassign.o -I${EXTRA_INCLUDE_PATH} -DROUTECALC=\"bin/routecalc\" src/ipassign.cc
 ${CC} ${COMPILE_FLAGS} -c -o tmp/bitmath.o -I${EXTRA_INCLUDE_PATH} -DROUTECALC=\"bin/routecalc\" src/bitmath.cc
-${CC} ${COMPILE_FLAGS} -c -o tmp/Assigner.o -I${EXTRA_INCLUDE_PATH} -DROUTECALC=\"bin/routecalc\" src/Assigner.cc
 ${CC} ${COMPILE_FLAGS} -c -o tmp/ConservativeAssigner.o -I${EXTRA_INCLUDE_PATH} -DROUTECALC=\"bin/routecalc\" src/ConservativeAssigner.cc
 ${CC} ${COMPILE_FLAGS} -c -o tmp/HierarchicalAssigner.o -I${EXTRA_INCLUDE_PATH} -DROUTECALC=\"bin/routecalc\" src/HierarchicalAssigner.cc
 ${CC} ${COMPILE_FLAGS} -c -o tmp/Framework.o -I${EXTRA_INCLUDE_PATH} -DROUTECALC=\"bin/routecalc\" src/Framework.cc
 ${CC} ${COMPILE_FLAGS} -c -o tmp/coprocess.o -I${EXTRA_INCLUDE_PATH} -DROUTECALC=\"bin/routecalc\" src/coprocess.cc
-${CC} ${LINK_FLAGS} -o bin/ipassign -L${EXTRA_LIB_PATH}/ tmp/ipassign.o tmp/bitmath.o tmp/Assigner.o tmp/ConservativeAssigner.o tmp/HierarchicalAssigner.o tmp/Framework.o tmp/coprocess.o -lmetis -lm
+${CC} ${LINK_FLAGS} -o bin/ipassign -L${EXTRA_LIB_PATH}/ tmp/ipassign.o tmp/bitmath.o tmp/ConservativeAssigner.o tmp/HierarchicalAssigner.o tmp/Framework.o tmp/coprocess.o -lmetis -lm
 
 #${CC} -o bin/difference src/difference.cc
 #${CC} -o bin/add-x src/add-x.cc
@@ -26,3 +25,4 @@ ${CC} -o bin/graph2single-source src/graph2single-source.cc -lm
 ${CC} -o bin/single-source src/single-source.cc -lm
 
 ${CC} -o bin/route2dist src/route2dist.cc -lm
+${CC} -o bin/route2lan src/route2lan.cc -lm
