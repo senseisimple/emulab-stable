876630e50fbb5e8cf9990d97604aebc3614ae0f7
David Johnson <johnsond@flux.utah.edu>
2006-12-01 23:09:12 +0000

    Copyrights.

--- robots/mtp/mtp.h
+++ robots/mtp/mtp.h
@@ -1,6 +1,6 @@
 /*
  * EMULAB-COPYRIGHT
- * Copyright (c) 2004, 2005 University of Utah and the Flux Group.
+ * Copyright (c) 2004-2006 University of Utah and the Flux Group.
  * All rights reserved.
  */
 
--- robots/tracker/Editor.java
+++ robots/tracker/Editor.java
@@ -1,3 +1,9 @@
+/*
+ * EMULAB-COPYRIGHT
+ * Copyright (c) 2005-2006 University of Utah and the Flux Group.
+ * All rights reserved.
+ */
+
 import java.util.*;
 
 
