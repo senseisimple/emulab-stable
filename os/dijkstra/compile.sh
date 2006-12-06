d679f7ccc346a2ad3caf279ea753596ac334ba24
Mike Hibler <mike@flux.utah.edu>
2006-12-06 16:28:05 +0000

    Remove some unnecessary scripts

--- os/dijkstra/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-g++33 -ftemplate-depth-30 -O3 -I/home/duerig/include -o dijk Compressor.cc \
-    NoneCompressor.cc SingleSource.cc TreeCompressor.cc \
-    VoteIpTree.cc bitmath.cc dijkstra.cc OptimalIpTree.cc
-
-g++33 -O3 -o check check-dijkstra.cc bitmath.cc
-
-g++33 -O3 -o route-histogram route-histogram.cc
