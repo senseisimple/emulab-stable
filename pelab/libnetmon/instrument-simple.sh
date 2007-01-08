a3ff98069278523fd4813165eaabe38c8bc2b65c
Robert Ricci <ricci@cs.utah.edu>
2007-01-08 22:48:40 +0000

    Turn on UDP monitoring

--- pelab/libnetmon/instrument-simple.sh
+++ pelab/libnetmon/instrument-simple.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 export LD_PRELOAD=libnetmon.so
+export LIBNETMON_MONITORUDP=1
 if [ "x$LIBNETMON_OUTPUTVERSION" = "x" ]; then
     export LIBNETMON_OUTPUTVERSION=3
 else
