7ef57e6a3ae69db3fc7874310f6d2a3a1fdd4910
Leigh B. Stoller <stoller@flux.utah.edu>
2007-02-12 21:19:27 +0000

    Seeing as Mike's club is now up to three members, lets add a confirm button.

--- www/pagenotworking.php
+++ www/pagenotworking.php
@@ -11,28 +11,45 @@ include("defs.php3");
 #
 PAGEHEADER("Page Not Working Properly!");
 
+$optargs = OptionalPageArguments("confirmed", PAGEARG_BOOLEAN,
+				 "referrer",  PAGEARG_STRING);
+
 $this_user = CheckLogin($check_status);
-$referrer  = $_SERVER['HTTP_REFERER'];
+$referrer  = (isset($referrer) ? $referrer : $_SERVER['HTTP_REFERER']);
 
 if ($this_user) {
-    $uid_name  = $this_user->name();
-    $uid_email = $this_user->email();
-    $uid_uid   = $this_user->uid();
+    if (!isset($confirmed)) {
+	echo "Are you sure you want to report that something is wrong with
+              <b>$referrer</b><br><br>";
 
-    TBMAIL($TBMAIL_OPS,
-	   "Page Not Working Properly",
-	   "$uid_name ($uid_uid) is reporting that page:\n\n".
-	   "    $referrer\n\n".
-	   "is not working properly",
-	   "From: $uid_name <$uid_email>\n".
-	   "Errors-To: $TBMAIL_WWW");
+	echo "<center>\n";
+ 	echo "<form action='pagenotworking.php' method=get>";
+	echo "<b><input type=hidden name=confirmed value=1></b>";
+	echo "<b><input type=hidden name=referrer value='$referrer'></b>";
+	echo "<b><input type=submit name=tag value='Submit'></b>";
+	echo "</form>";
+	echo "</center>\n";
+    }
+    else {
+	$uid_name  = $this_user->name();
+	$uid_email = $this_user->email();
+	$uid_uid   = $this_user->uid();
 
-    echo "<br>
+	TBMAIL($TBMAIL_OPS,
+	       "Page Not Working Properly",
+	       "$uid_name ($uid_uid) is reporting that page:\n\n".
+	       "    $referrer\n\n".
+	       "is not working properly",
+	       "From: $uid_name <$uid_email>\n".
+	       "Errors-To: $TBMAIL_WWW");
+
+	echo "<br>
          Thanks! A message has been sent to $TBMAILADDR to let us know
          something might be wrong with <b>$referrer</b>";
-
-    echo "<br><br><br>\n";
+    }
 }
+
+echo "<br><br><br>\n";
 echo "Back to <a href='$referrer'>previous page</a>\n";
 echo "<br>\n";
 
