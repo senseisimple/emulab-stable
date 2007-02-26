b58f355275de0666c52afafcf573006ebde6b1f5
Leigh B. Stoller <stoller@flux.utah.edu>
2007-02-26 14:41:15 +0000

    Now that things have settled down, add a text box to the pagenotworking page so that people can tell us what is wrong!

--- www/pagenotworking.php
+++ www/pagenotworking.php
@@ -11,19 +11,24 @@ include("defs.php3");
 #
 PAGEHEADER("Page Not Working Properly!");
 
-$optargs = OptionalPageArguments("confirmed", PAGEARG_BOOLEAN,
-				 "referrer",  PAGEARG_STRING);
+$optargs = OptionalPageArguments("confirmed",   PAGEARG_BOOLEAN,
+				 "description", PAGEARG_STRING, 
+				 "referrer",    PAGEARG_STRING);
 
 $this_user = CheckLogin($check_status);
 $referrer  = (isset($referrer) ? $referrer : $_SERVER['HTTP_REFERER']);
 
 if ($this_user) {
     if (!isset($confirmed)) {
-	echo "Are you sure you want to report that something is wrong with
+	echo "<center>";
+	echo "Are you sure you want to report a problem with:<br>
               <b>$referrer</b><br><br>";
 
-	echo "<center>\n";
  	echo "<form action='pagenotworking.php' method=get>";
+
+	echo "<b>Please tell us briefly what is wrong ...</b>\n";
+	echo "<br>\n";
+	echo "<textarea name=description rows=10 cols=80></textarea><br>\n";
 	echo "<b><input type=hidden name=confirmed value=1></b>";
 	echo "<b><input type=hidden name=referrer value='$referrer'></b>";
 	echo "<b><input type=submit name=tag value='Submit'></b>";
@@ -35,17 +40,21 @@ if ($this_user) {
 	$uid_email = $this_user->email();
 	$uid_uid   = $this_user->uid();
 
+	if (! isset($description))
+	    $description = "";
+
 	TBMAIL($TBMAIL_OPS,
 	       "Page Not Working Properly",
 	       "$uid_name ($uid_uid) is reporting that page:\n\n".
 	       "    $referrer\n\n".
-	       "is not working properly",
+	       "is not working properly:\n\n".
+	       "$description\n",
 	       "From: $uid_name <$uid_email>\n".
 	       "Errors-To: $TBMAIL_WWW");
 
 	echo "<br>
          Thanks! A message has been sent to $TBMAILADDR to let us know
-         something might be wrong with <b>$referrer</b>";
+         something is wrong with <b>$referrer</b>";
     }
 }
 
