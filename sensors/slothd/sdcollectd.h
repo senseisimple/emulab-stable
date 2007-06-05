9b0ab52cc347eacce3aa9189128341bb8c1d9e05
Leigh B. Stoller <stoller@flux.utah.edu>
2007-06-05 22:30:12 +0000

    Minor bug fix, reported by Michael Blodgett.

--- sensors/slothd/sdcollectd.h
+++ sensors/slothd/sdcollectd.h
@@ -1,6 +1,6 @@
 /*
  * EMULAB-COPYRIGHT
- * Copyright (c) 2000-2003 University of Utah and the Flux Group.
+ * Copyright (c) 2000-2003, 2007 University of Utah and the Flux Group.
  * All rights reserved.
  */
 
@@ -25,7 +25,7 @@
 #include <syslog.h>
 #include <pwd.h>
 #include <grp.h>
-#include <tbdb.h>
+#include "tbdb.h"
 #include "log.h"
 
 #define SDPROTOVERS 2
