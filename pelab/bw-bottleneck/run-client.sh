e1efdcccefb88006b06bdf5f609f7551e9b79675
Jonathon Lynn Duerig <duerig@flux.utah.edu>
2007-08-03 02:41:33 +0000

    Updated to use filter for smaller tcpdump output.

--- pelab/bw-bottleneck/run-client.sh
+++ pelab/bw-bottleneck/run-client.sh
@@ -2,6 +2,6 @@
 
 # Usage: run-client.sh <dest1-ip> <dest2-ip> <port> <base-path> <output-file> <duration>
 
-sudo /usr/sbin/tcpdump -i vnet dst port $3 > /local/logs/$5 &
+sudo /usr/sbin/tcpdump -n -tt -i vnet dst port $3 | perl $4/glean.pl > /local/logs/$5 &
 $4/iperf -c $1 -p $3 -t $6&
 $4/iperf -c $2 -p $3 -t $6
--- pelab/bw-bottleneck/run-server.sh
+++ pelab/bw-bottleneck/run-server.sh
@@ -2,5 +2,5 @@
 
 # Usage: run-server.sh <port> <base-path> <output-file>
 
-sudo /usr/sbin/tcpdump -i vnet dst port $1 > /local/logs/$3 &
+sudo /usr/sbin/tcpdump -n -tt -i vnet dst port $1 | perl $2/glean.pl > /local/logs/$3 &
 $2/iperf -s -p $1
