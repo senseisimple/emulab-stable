5c7271c93e4297050bf02a8affa2b43af18d278a
Kevin Atkinson <kevina@cs.utah.edu>
2007-09-20 22:52:37 +0000

    Don't hard code proj and exp name.

--- pelab/init-by-file.pl
+++ pelab/init-by-file.pl
@@ -12,8 +12,8 @@ open(FILE, "<".$filename) or die("Cannot open $filename\n");
 while ($line = <FILE>)
 {
     $line =~ /^10.0.0.([0-9]+) ([0-9.]+) ([0-9.]+) ([0-9.]+) [0-9.]+$/;
-    print("/usr/testbed/bin/tevc -e tbres/pelab-generated now elabc-elab-$1 modify dest=$2 bandwidth=$3\n");
-    print("/usr/testbed/bin/tevc -e tbres/pelab-generated now elabc-elab-$1 modify dest=$2 delay=$4\n");
-    system("/usr/testbed/bin/tevc -e tbres/pelab-generated now elabc-elab-$1 modify dest=$2 bandwidth=$3");
-    system("/usr/testbed/bin/tevc -e tbres/pelab-generated now elabc-elab-$1 modify dest=$2 delay=$4");
+    print("/usr/testbed/bin/tevc -e $proj/$exp now elabc-elab-$1 modify dest=$2 bandwidth=$3\n");
+    print("/usr/testbed/bin/tevc -e $proj/$exp now elabc-elab-$1 modify dest=$2 delay=$4\n");
+    system("/usr/testbed/bin/tevc -e $proj/$exp now elabc-elab-$1 modify dest=$2 bandwidth=$3");
+    system("/usr/testbed/bin/tevc -e $proj/$exp now elabc-elab-$1 modify dest=$2 delay=$4");
 }
