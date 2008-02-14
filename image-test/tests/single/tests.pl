bcabe386e9fed538ecd967a8fa146b729941ff34
Kevin Atkinson <kevina@cs.utah.edu>
2008-02-14 07:17:30 +0000

    Add pc2400c2 and pc2000w to parms list.
    Add basic test of the event system and program agents.

--- image-test/tests/single/tests.pl
+++ image-test/tests/single/tests.pl
@@ -10,5 +10,9 @@ test_rcmd 'tgz', ['tar'], 'node', 'test -e /usr/tgz/hw.txt';
 test_rcmd 'tar.gz', ['tar'], 'node', 'test -e /usr/tar.gz/hw.txt';
 test_rcmd 'tar.bz2', ['tar'], 'node', 'test -e /usr/tar.bz2/hw.txt';
 test_rcmd 'startcmd', [], 'node', 'test -e /tmp/startcmd-ok';
+print "Sleeping 5 seconds...\n";
+sleep 5;
+test_rcmd 'prog_simple', [], 'node', 'test -e /tmp/prog_simple-ok';
+test_rcmd 'prog_env', [], 'node', "test -e /tmp/testenv-$parms{pid}-$parms{eid}";
 test_cmd 'reboot', [], "node_reboot -w -e $parms{pid},$parms{eid}";
 
