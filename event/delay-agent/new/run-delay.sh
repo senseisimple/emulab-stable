8543b5d9b0a88f42804105175770d77998ff2a24
Jonathon Lynn Duerig <duerig@flux.utah.edu>
2008-04-14 04:01:40 +0000

    Added a file which automatically runs the delay agent on FreeBSD with the right options

---
+++ event/delay-agent/new/run-delay.sh
@@ -0,0 +1,4 @@
+
+echo ./delay-agent -E $1/$2 -f /var/emulab/boot/delay_mapping -s localhost -k /var/emulab/boot/eventkey -i delay-agent.pid
+./delay-agent -E $1/$2 -f /var/emulab/boot/delay_mapping -s localhost -k /var/emulab/boot/eventkey -i delay-agent.pid
+
