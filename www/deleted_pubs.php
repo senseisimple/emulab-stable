6e208b09ab1269546d18882666024c9e0a7fd3c9
Kevin Atkinson <kevina@cs.utah.edu>
2008-06-11 22:09:04 +0000

    Add missing file.

---
+++ www/deleted_pubs.php
@@ -0,0 +1,32 @@
+<?php
+include("pub_defs.php");
+
+#
+# Only known and logged in users.
+#
+$this_user = CheckLoginOrDie();
+$uid_idx   = $this_user->uid_idx();
+$isadmin   = ISADMIN();
+$optargs   = OptionalPageArguments("showdeleted", PAGEARG_BOOLEAN);
+
+PAGEHEADER("Deleted Publications");
+?>
+
+<p>To undelete a publication simply edit it again and unclick "Mark As Deleted"</p>
+
+<?php
+
+$where_clause = '';
+$deleted_clause = '`deleted`';
+
+if ($isadmin) {
+  $where_clause = '1';
+} else {
+  $where_clause = "(`owner` = $uid_idx or `last_edit_by` = $uid_idx)";
+}
+
+$query_result = GetPubs($where_clause, $deleted_clause);
+echo MakeBibList($this_user, $isadmin, $query_result);
+
+PAGEFOOTER();
+?>
