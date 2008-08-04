0a46d1b0be8b592c5bedc9f95d707a2bd21ae5e2
Leigh B. Stoller <stoller@flux.utah.edu>
2008-08-04 19:06:33 +0000

    Wrapper for our google CSE search to deal with the http vs https problem; google rejects https, and using http results in mixed content warnings from the browsers.

---
+++ www/search_cse.php
@@ -0,0 +1,18 @@
+<?php
+#
+# EMULAB-COPYRIGHT
+# Copyright (c) 2000-2008 University of Utah and the Flux Group.
+# All rights reserved.
+#
+require("defs.php3");
+
+#
+# Verify page arguments.
+#
+$optargs = OptionalPageArguments("submit",      PAGEARG_STRING,
+				 "query",       PAGEARG_STRING);
+
+header("Location: http://www.google.com/cse?cx=007876920815929749329:hb-sxxwdr8y&ie=UTF-8&q=$query&sa=\"Search+Documentation\"");
+
+?>
+
