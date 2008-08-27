85aaeaa0fbf01f6f185bc2d7811f4aa4028bacd7
David Johnson <johnsond@flux.utah.edu>
2008-08-27 21:28:18 +0000

    Make sure to cp the pems too -- the client one is different for widearea.

--- tmcd/widearea/cpwa.sh
+++ tmcd/widearea/cpwa.sh
@@ -49,6 +49,8 @@ if [ -e $ETCDIR/isrem ]; then
     cp -p $ETCDIR/bossnode $METCDIR/bossnode
     cp -p $ETCDIR/emulab-privkey $METCDIR/emulab-privkey
     cp -p $ETCDIR/waconfig $METCDIR/waconfig
+    cp -p $ETCDIR/client.pem $METCDIR/client.pem
+    cp -p $ETCDIR/emulab.pem $METCDIR/emulab.pem
 fi
 
 exit 0
