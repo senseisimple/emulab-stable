1229a9a6bd69326618831f2f555800c9f2cf9fd8
Robert Ricci <ricci@cs.utah.edu>
2009-04-29 22:15:58 +0000

    Expand Kevin's text a bit. The text is very Utah-specific, but I don't beleive we really have this stuff set up to run well at other sites.

--- node_usage/index.php
+++ node_usage/index.php
@@ -13,14 +13,30 @@ PAGEHEADER("Testbed Node Usage Stats");
 <p>
 
 These graphs show the average number of free nodes over various time
-periods. Hourly graphs show the average number of free nodes in the
-given hour. Daily graphs show the average number of free nodes in the
-given day. Etc.
+periods. Nodes are broken down into two classes: The first class are the 
+<a href="http://users.emulab.net/trac/emulab/wiki/pc3000">pc3000</a>s,
+which are the most numerous, and are fairly modern. They are
+most experimenters' nodes of choice. For many experimenters, the number of
+free pc3000s limits the size of the experiments they can run.
+The second class includes other, older PCs (along with the pc3000s) to give
+a sense for how many nodes are available if one is willing to use some
+slower nodes.
+
+</p>
 
 <p>
+Hourly graphs show the average number of free nodes in the
+given hour. Daily graphs show the average number of free nodes in the
+given day, etc. Note that because some data in these graphs is averaged
+over a very long time period (up to four years), it may not reflect recent
+trends.
+</p>
+
+
+
+<h2 align=center>Recent Availability</h2>
 
 <p align=center>
-
 <b>Last 2 Weeks (hourly)</b>
 <br>
 <img src="node_avail-hourly_last2weeks.png">
@@ -33,16 +49,20 @@ given day. Etc.
 
 <p align=center>
 <hr>
-<p align=center>
 
-<b>A Typical Week (hourly)</b>
+<h2 align=center>Diurnal</h2>
+
+<p align=center>
+<b>A Typical Week (hourly, average since 2005)</b>
 <br>
 <img src="node_avail-by_hourofweek.png">
 
 <p align=center>
 <hr>
-<p align=center>
 
+<h2 align=center>Long-Term Trends</h2>
+
+<p align=center>
 <b>Since Sep 2005 (yearly)</b>
 
 <br>
