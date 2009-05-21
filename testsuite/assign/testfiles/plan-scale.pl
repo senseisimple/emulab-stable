42629331834e11eeeaa9208da8d1d7d744e12d48
Robert Ricci <ricci@cs.utah.edu>
2009-05-21 17:53:11 +0000

    Initial checkin of a simple test harness for running assign and gathering statistics from its output. Code is well commented, but I still need to write some overall documentation.

---
+++ testsuite/assign/testfiles/plan-scale.pl
@@ -0,0 +1,4 @@
+@tops = ( "plan-20.top" );
+@ptops = &::lsptop("big-*.ptop");
+$name = "plan20-scale";
+$assignopts = "-P";
---
+++ testsuite/assign/testfiles/plan-small-scale.pl
@@ -0,0 +1,4 @@
+@tops = ( "plan-20.top" );
+@ptops = &::lsptop("small-*.ptop");
+$name = "plan20-small-scale";
+$assignopts = "-P";
