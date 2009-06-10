0763d8cedf846af45fb3db435065d3f9feee4f55
Kevin Atkinson <kevina@cs.utah.edu>
2009-06-10 22:06:20 +0000

    Generalize node_usage plots to work on other sits, after some initial setup, see http://users.emulab.net/trac/emulab/wiki/NodeUsageInstall.

--- node_usage/index.php
+++ node_usage/index.php
@@ -10,34 +10,7 @@ PAGEHEADER("Testbed Node Usage Stats");
 
 ?>
 
-<p>
-
-These graphs show the average number of free nodes over various time
-periods. Nodes are broken down into two classes: The first class are the 
-<a href="http://users.emulab.net/trac/emulab/wiki/pc3000">pc3000</a>s,
-which are the most numerous, and are fairly modern. They are
-most experimenters' nodes of choice. For many experimenters, the number of
-free pc3000s limits the size of the experiments they can run.
-The second class includes other, older PCs (along with the pc3000s) to give
-a sense for how many nodes are available if one is willing to use some
-slower nodes.
-</p>
-
-<p>
-Emulab currently includes around 350 PC nodes. As you can see, most of those
-are in use even during our periods of &quot;light&quot; usage.
-</p>
-
-<p>
-Hourly graphs show the average number of free nodes in the
-given hour. Daily graphs show the average number of free nodes in the
-given day, etc. All times are given in Mountain Time. Note that because
-some data in these graphs is averaged over a very long time period (up to four
-years), it may not reflect recent
-trends.
-</p>
-
-
+$avail_header
 
 <h2 align=center>Recent Availability</h2>
 
@@ -89,8 +62,7 @@ trends.
 
 <p align=center>
 
-Note: The gap in the last two graphs represents periods when no data
-was availabe due to bugs in our system.
+$avail_footer
 
 <?php
 
