6f29bab10aae013be5b7a5ac949eb648f531c163
Leigh B. Stoller <stoller@flux.utah.edu>
2009-06-11 21:41:42 +0000

    Add the script locking code we use on boss, for use from the openvz library where we have to serialize access to global resources.

--- tmcd/common/libtestbed.pm
+++ tmcd/common/libtestbed.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -wT
 #
 # EMULAB-COPYRIGHT
-# Copyright (c) 2000-2004 University of Utah and the Flux Group.
+# Copyright (c) 2000-2009 University of Utah and the Flux Group.
 # All rights reserved.
 #
 
@@ -12,10 +12,16 @@
 package libtestbed;
 use Exporter;
 @ISA    = "Exporter";
-@EXPORT = qw( TB_BOSSNODE TB_EVENTSERVER );
+@EXPORT = qw( TB_BOSSNODE TB_EVENTSERVER
+	      TBScriptLock TBScriptUnlock 
+	      TBSCRIPTLOCK_OKAY TBSCRIPTLOCK_TIMEDOUT
+	      TBSCRIPTLOCK_IGNORE TBSCRIPTLOCK_FAILED TBSCRIPTLOCK_GLOBALWAIT
+	    );
 
 # Must come after package declaration!
 use English;
+# For locking below
+use Fcntl ':flock';
 
 #
 # Turn off line buffering on output
@@ -72,6 +78,151 @@ sub TB_EVENTSERVER()
     }
 
     return $eventserver;
+}
+
+#
+# Serialize an operation (script).
+#
+my $lockname;
+my $lockhandle;
+
+# Return Values.
+sub TBSCRIPTLOCK_OKAY()		{ 0;  }
+sub TBSCRIPTLOCK_TIMEDOUT()	{ 1;  }
+sub TBSCRIPTLOCK_IGNORE()	{ 2;  }
+sub TBSCRIPTLOCK_FAILED()	{ -1; }
+sub TBSCRIPTLOCK_GLOBALWAIT()	{ 1; }
+
+# 
+# There are two kinds of serialization.
+#
+#   * Usual Kind: Each party just waits for a chance to go.
+#   * Other Kind: Only the first party really needs to run; the others just
+#                 need to wait. For example; exports_setup operates globally,
+#                 so there is no reason to run it more then once. We just
+#                 need to make sure that everyone waits for the one that is
+#		  running to finish. Use the global option for this.
+#
+sub TBScriptLock($;$$)
+{
+    my ($token, $global, $waittime) = @_;
+    local *LOCK;
+
+    if (!defined($waittime)) {
+	$waittime = 30;
+    }
+    elsif ($waittime == 0) {
+	$waittime = 99999999;
+    }
+    $global = 0
+	if (defined($global) || $global != TBSCRIPTLOCK_GLOBALWAIT());
+    $lockname = "/var/tmp/testbed_${token}_lockfile";
+
+    my $oldmask = umask(0000);
+
+    if (! open(LOCK, ">>$lockname")) {
+	print STDERR "Could not open $lockname!\n";
+	umask($oldmask);
+	return TBSCRIPTLOCK_FAILED();
+    }
+    umask($oldmask);
+
+    if (! $global) {
+	#
+	# A plain old serial lock.
+	#
+	while (flock(LOCK, LOCK_EX|LOCK_NB) == 0) {
+	    print "Another $token is in progress. Waiting a moment ...\n";
+
+	    $waittime--;
+	    if ($waittime == 0) {
+		print STDERR "Could not get the lock after a long time!\n";
+		return TBSCRIPTLOCK_TIMEDOUT();
+	    }
+	    sleep(1);
+	}
+	# Okay, got the lock. Save the handle. We need it below.
+	$lockhandle = *LOCK;
+	return TBSCRIPTLOCK_OKAY();
+    }
+
+    #
+    # Okay, a global lock.
+    #
+    # If we don't get it the first time, we wait for:
+    # 1) The lock to become free, in which case we do our thing
+    # 2) The time on the lock to change, in which case we wait for that 
+    #    process to finish, and then we are done since there is no
+    #    reason to duplicate what the just finished process did.
+    #
+    if (flock(LOCK, LOCK_EX|LOCK_NB) == 0) {
+	my $oldlocktime = (stat(LOCK))[9];
+	my $gotlock = 0;
+	
+	while (1) {
+	    print "Another $token in progress. Waiting a moment ...\n";
+	    
+	    if (flock(LOCK, LOCK_EX|LOCK_NB) != 0) {
+		# OK, got the lock
+		$gotlock = 1;
+		last;
+	    }
+	    my $locktime = (stat(LOCK))[9];
+	    if ($locktime != $oldlocktime) {
+		$oldlocktime = $locktime;
+		last;
+	    }
+	    
+	    $waittime--;
+	    if ($waittime <= 0) {
+		print STDERR "Could not get the lock after a long time!\n";
+		return TBSCRIPTLOCK_TIMEDOUT();
+	    }
+	    sleep(1);
+	}
+
+	$count = 0;
+	#
+	# If we did not get the lock, wait for the process that did to finish.
+	#
+	if (!$gotlock) {
+	    while (1) {
+		if ((stat(LOCK))[9] != $oldlocktime) {
+		    return TBSCRIPTLOCK_IGNORE();
+		}
+		if (flock(LOCK, LOCK_EX|LOCK_NB) != 0) {
+		    close(LOCK);
+		    return TBSCRIPTLOCK_IGNORE();
+		}
+
+		$waittime--;
+		if ($waittime <= 0) {
+		    print STDERR
+			"Process with the lock did not finish after ".
+			"a long time!\n";
+		    return TBSCRIPTLOCK_TIMEDOUT();
+		}
+		sleep(1); 
+	    }
+	}
+    }
+    #
+    # Perl-style touch(1)
+    #
+    my $now = time;
+    utime $now, $now, $lockname;
+    
+    $lockhandle = *LOCK;
+    return TBSCRIPTLOCK_OKAY();
+}
+
+#
+# Unlock; Just need to close the file (releasing the lock).
+#
+sub TBScriptUnlock()
+{
+    close($lockhandle)
+	if defined($lockhandle);
 }
 
 1;
