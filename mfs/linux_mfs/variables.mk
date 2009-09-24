5d39aa1de21521b25e400d46de362c1756c83f9e
Ryan Jackson <rdjackso@flux.utah.edu>
2009-09-24 00:19:05 +0000

    Lots of makefile changes
    * Moved tarballs to a single directory
    * More 64-bit related changes

--- mfs/linux_mfs/variables.mk
+++ mfs/linux_mfs/variables.mk
@@ -2,6 +2,7 @@ TOPDIR			=	$(PWD)
 SOURCE_PATH		=	$(TOPDIR)/source
 SCRIPTS_PATH		=	$(TOPDIR)/scripts
 TOOLCHAIN_BUILD_PATH	=	$(TOPDIR)/toolchain_build
+TARBALL_PATH		=	$(TOPDIR)/tarballs
 
 TARGET_BUILD_PATH	=	$(TOPDIR)/build
 TARGET_PATH		=	$(TOPDIR)/target
