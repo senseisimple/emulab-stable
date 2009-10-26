990de36fba829a4a8583dd841d19878a89d9a2e5
Leigh B. Stoller <stoller@flux.utah.edu>
2009-10-26 16:58:34 +0000

    Fill in the "minimal" part of the version two CM API, now functional.
    * Most of the changes in GeniCM are simple reorganizations to allow me
      to call into the code from the GeniCMV2 module. This is to avoid
      code duplication as much as possible. Eventually, I will have to
      duplicate the code, but for the moment this works okay.
    
    * I am ignoring the slice_urn argument for now. Will add that later.
    
    * See protogeni/test/version2 for the version two test cases.

---
+++ protogeni/test/version2/deletesliver.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+#
+# GENIPUBLIC-COPYRIGHT
+# Copyright (c) 2008-2009 University of Utah and the Flux Group.
+# All rights reserved.
+# 
+# Permission to use, copy, modify and distribute this software is hereby
+# granted provided that (1) source code retains these copyright, permission,
+# and disclaimer notices, and (2) redistributions including binaries
+# reproduce the notices in supporting documentation.
+#
+# THE UNIVERSITY OF UTAH ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
+# CONDITION.  THE UNIVERSITY OF UTAH DISCLAIMS ANY LIABILITY OF ANY KIND
+# FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+#
+
+#
+#
+import sys
+import pwd
+import getopt
+import os
+import re
+
+ACCEPTSLICENAME=1
+
+execfile( "../test-common.py" )
+
+#
+# Get a credential for myself, that allows me to do things at the SA.
+#
+mycredential = get_self_credential()
+print "Got my SA credential. Looking for slice ..."
+
+#
+# Lookup slice, delete before proceeding.
+#
+myslice = resolve_slice( SLICENAME, mycredential )
+print "Found the slice, asking for a credential ..."
+
+#
+# Get the slice credential.
+#
+slicecred = get_slice_credential( myslice, mycredential )
+print "Got the slice credential, asking for a sliver credential ..."
+
+#
+# Get the sliver credential.
+#
+params = {}
+params["credentials"] = (slicecred,)
+rval,response = do_method("cmv2", "GetSliver", params)
+if rval:
+    Fatal("Could not get Sliver credential")
+    pass
+slivercred = response["value"]
+print "Got the sliver credential, deleting the sliver";
+
+#
+# Delete the sliver.
+#
+params = {}
+params["credentials"] = (slivercred,)
+rval,response = do_method("cmv2", "DeleteSliver", params)
+if rval:
+    Fatal("Could not delete sliver")
+    pass
+print "Sliver has been deleted."
+
+
---
+++ protogeni/test/version2/sliverstatus.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+#
+# GENIPUBLIC-COPYRIGHT
+# Copyright (c) 2008-2009 University of Utah and the Flux Group.
+# All rights reserved.
+# 
+# Permission to use, copy, modify and distribute this software is hereby
+# granted provided that (1) source code retains these copyright, permission,
+# and disclaimer notices, and (2) redistributions including binaries
+# reproduce the notices in supporting documentation.
+#
+# THE UNIVERSITY OF UTAH ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
+# CONDITION.  THE UNIVERSITY OF UTAH DISCLAIMS ANY LIABILITY OF ANY KIND
+# FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+#
+
+#
+#
+import sys
+import pwd
+import getopt
+import os
+import re
+
+ACCEPTSLICENAME=1
+
+execfile( "../test-common.py" )
+
+#
+# Get a credential for myself, that allows me to do things at the SA.
+#
+mycredential = get_self_credential()
+print "Got my SA credential. Looking for slice ..."
+
+#
+# Lookup slice.
+#
+myslice = resolve_slice( SLICENAME, mycredential )
+print "Found the slice, asking for a credential ..."
+
+#
+# Get the slice credential.
+#
+slicecred = get_slice_credential( myslice, mycredential )
+print "Got the slice credential, asking for a sliver credential ..."
+
+#
+# Get the sliver credential.
+#
+params = {}
+params["credentials"] = (slicecred,)
+rval,response = do_method("cmv2", "GetSliver", params)
+if rval:
+    Fatal("Could not get Sliver credential")
+    pass
+slivercred = response["value"]
+print "Got the sliver credential, asking for sliver status";
+
+#
+# Get the sliver status
+#
+params = {}
+params["credentials"] = (slivercred,)
+rval,response = do_method("cmv2", "SliverStatus", params)
+if rval:
+    Fatal("Could not get sliver status")
+    pass
+print str(response["value"])
+
