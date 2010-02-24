f6c71abf7ba8d9f2c6fa67560428e24159816ebe
Mike Hibler <hibler@cs.utah.edu>
2010-02-24 10:58:18 -0700

    Major mkextrafs whackage for FreeBSD.
    Change mkextrafs so that we can create a filessytem on the second disk.
    The command line option was always there, it just didn't work.
    
    Added -l option to make it print out "available" partitions that you could
    put a filesystem on.  This uses the new os_find_freedisk() function which
    was inspired by Dave's Linux libvnode routine to locate unused space.
    In fact, that latter function should get moved up to linux/liblocsetup.pm
    and the Linux mkextrafs updated to support -l.

--- tmcd/freebsd/mkextrafs.pl
+++ tmcd/freebsd/mkextrafs.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #
 # EMULAB-COPYRIGHT
-# Copyright (c) 2000-2005, 2007 University of Utah and the Flux Group.
+# Copyright (c) 2000-2010 University of Utah and the Flux Group.
 # All rights reserved.
 #
 use English;
@@ -20,14 +20,24 @@ my $DOSTYPE = "$BINDIR/dostype";
 #
 sub usage()
 {
-    print("Usage: mkextrafs.pl [-f] [-s slice] [-r disk] <mountpoint>\n");
+    print("Usage: mkextrafs.pl [-fl] [-s slice] [-r disk] <mountpoint>\n");
+    print("       [-r disk]  disk device to use (default: ad0)\n");
+    print("       [-s slice] DOS partition to use or 0 for entire disk (default: 4)\n");
+    print("       <mount>    where the new filesystem will be mounted\n");
+    print("       -f         DANGEROUS! force creation of the filesystem\n");
+    print("                   even if partition is in use\n");
+    print("Usage: mkextrafs.pl -l [-f]\n");
+    print("       -l  list available partitions\n");
+    print("       -f  also list inactive partitions that have non-zero type\n");
     exit(-1);
 }
-my $optlist    = "fs:r:cn";
+my $optlist    = "fls:r:cn";
 my $diskopt;
 my $checkit    = 0;
 my $forceit    = 0;
 my $noinit     = 0;
+my $showonly   = 0;
+my $debug      = 0;
 
 #
 # Yep, hardwired for now.  Should be options or queried via TMCC.
@@ -37,6 +47,8 @@ my $slice      = "4";
 my $partition  = "e";
 
 sub mysystem($);
+sub showspace($);
+sub os_find_freedisk($$); # XXX from liblocsetup.pm
 
 #
 # Turn off line buffering on output
@@ -77,6 +89,15 @@ if (defined($options{"r"})) {
 if (defined($options{"n"})) {
     $noinit = $options{"n"};
 }
+if (defined($options{"l"})) {
+    $showonly = 1;
+}
+
+if ($showonly) {
+    showspace($forceit);
+    exit(0);
+}
+
 if (@ARGV != 1) {
     usage();
 }
@@ -104,6 +125,15 @@ my $slicedev   = "${disk}s${slice}";
 my $fsdevice   = "/dev/${slicedev}${partition}";
 
 #
+# For entire disk, we will use "fdisk -I" to initialize DOS partition 1.
+# Note: we will create the BSD 'e' partition later.
+#
+if ($slice == 0) {
+    $slicedev = "${disk}s1";
+    $fsdevice = "/dev/${slicedev}e";
+}
+
+#
 # An existing fstab entry indicates we have already done this
 # XXX override with forceit?  Would require unmounting and removing from fstab.
 #
@@ -127,13 +157,59 @@ if ($mounted =~ /^${fsdevice} on (\S*)/) {
 	"    $fsdevice is already mounted on $1\n");
 }
 
-my $slicesetup = `fdisk -s ${disk} | grep '^[ ]*${slice}:'`;
-if ($slicesetup =~ /^[ ]*${slice}:\s*(\d*)\s*(\d*)\s*(0x\S\S)\s*/) {
-    $stype = hex($3);
-}
-else {
+#
+# See what the current type is for the partition
+#
+my $stype = -1;
+if (!open(FD, "fdisk -s $disk 2>&1|")) {
     die("*** $0:\n".
-    "    Could not parse slice $slice fdisk entry!\n");
+        "    $disk: could not get partition info\n");
+}
+while (<FD>) {
+    #
+    # No MBR should mean that the entire disk is unused.
+    # If they want to use the whole disk (slice==0), this is okay.
+    #
+    if (/invalid fdisk partition table found/) {
+	if ($slice != 0) {
+	    die("*** $0:\n".
+		"    $disk: no partition table!\n");
+	}
+	$stype = 0;
+    }
+    #
+    # Format of fdisk output is:
+    #  /dev/da0: 30394 cyl 255 hd 63 sec
+    #  Part        Start Size     Type Flags
+    #  1:          63    12305790 0xa5 0x80
+    #  ...
+    #
+    if (/^\s*([1-4]):\s+\d+\s+\d+\s+(0x\S\S)\s+/) {
+	my $part = $1;
+	my $type = hex($2);
+	#
+	# If there is a valid partition on the disk and they are
+	# using the entire disk without forcing, stop them!
+	#
+	if ($slice == 0) {
+	    if ($type != 0 && !$forceit) {
+		die("*** $0:\n".
+		    "    There are valid partitions on the disk;".
+		    " need to specify -f to overwrite entire disk!\n");
+	    }
+	    $stype = 0;
+	}
+	if ($slice == $part) {
+	    $stype = $type;
+	    last;
+	}
+    }
+}	    
+close(FD);
+
+if ($stype == -1) {
+    die("*** $0:\n".
+        "    Could not find partition $slice fdisk entry!\n");
 }
 
 #
@@ -150,14 +226,24 @@ if (!$forceit) {
 }
 
 #
-# Set the partition type to BSD if not already set.
+# If they are whacking the whole disk, just use "fdisk -I' to initialize
+# partition 1 of the disk.
 #
-if ($stype != 165) {
+if ($slice == 0 && !$noinit) {
+    mysystem("fdisk -I $disk");
+}
+#
+# Otherwise set the partition type to BSD if not already set.
+#
+elsif ($stype != 165) {
     die("*** $0:\n".
 	"    No $DOSTYPE program, cannot set type of DOS partition\n")
 	if (! -e "$DOSTYPE");
     mysystem("$DOSTYPE -f /dev/$disk $slice 165");
 }
+#
+# If not recreating the filesystems, just try to mount it
+#
 elsif ($noinit) {
     mysystem("mount $fsdevice $mountpoint");
     mysystem("echo \"$fsdevice $mountpoint ufs rw 0 2\" >> /etc/fstab");
@@ -234,4 +320,64 @@ sub mysystem($)
 	}
     }
     return 0
+}
+
+sub sizestr($)
+{
+    my ($bytes) = @_;
+    my ($divisor, $units);
+
+    if ($bytes > 1000 * 1000 * 1000 * 1000) {
+	$divisor = 1000 * 1000 * 1000 * 1000;
+	$units = "TB";
+    } elsif ($bytes > 1000 * 1000 * 1000) {
+	$divisor = 1000 * 1000 * 1000;
+	$units = "GB";
+    } else {
+	$divisor = 1000 * 1000;
+	$units = "MB";
+    }
+    return sprintf "%6.2f%s", $bytes / $divisor, $units;
+}
+
+sub showspace($)
+{
+    my ($force) = @_;
+
+    # XXX we keep this dynamic since this script can operate standalone
+    require liblocsetup;
+
+    my %diskinfo = liblocsetup::os_find_freedisk(0, $force);
+    if (!%diskinfo) {
+	print "No space found on node\n";
+    }
+    print "Found space";
+    print " ('*' = in use, but unmounted)"
+	if ($force);
+    print ":\nDisk\tSlice\tBytes\n";
+    foreach my $disk (sort keys %diskinfo) {
+	my $ff = "";
+	my $iu = "";
+	if (exists($diskinfo{$disk}{"size"})) {
+	    my $size = sizestr($diskinfo{$disk}{"size"});
+	    if ($diskinfo{$disk}{"type"} != 0) {
+		$ff = "-f";
+		$iu = "*";
+	    }
+	    print "$disk$iu\t \t$size # mkextrafs -r $disk -s 0 $ff\n";
+	} else {
+	    foreach my $part (keys %{$diskinfo{$disk}}) {
+		my $pnum;
+		($pnum = $part) =~ s/.*(\d+)$/$1/;
+		if (exists($diskinfo{$disk}{$part}{"size"})) {
+		    my $size = sizestr($diskinfo{$disk}{$part}{"size"});
+		    if ($diskinfo{$disk}{$part}{"type"} != 0) {
+			$ff = "-f";
+			$iu = "*";
+		    }
+		    print "$disk$iu\t$pnum\t$size # mkextrafs -r $disk -s $pnum $ff\n";
+		}
+	    }
+	}
+    }
 }
