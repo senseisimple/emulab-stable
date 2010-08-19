05e25a144653b10b597ba5901b63fa6f18782173
Robert Ricci <ricci@cs.utah.edu>
2010-08-19 10:44:23 -0600

    Make assign build with GCC 4.4.
    The main change is to add a NEWER_GCC macro, separate from NEW_GCC, to
    detect GCC 4.3 and up.
    
    This is necessary because, in GCC 4.3, hash_fun.h moved from ext/ to
    backward/.  New comments in port.h give the details.
    
    common.h, fstring.h: use NEWER_GCC to locate hash_fun.h.  port.h:
    define NEWER_GCC if GCC >= 4.3.

--- assign/port.h
+++ assign/port.h
@@ -17,9 +17,23 @@
 /*
  * We have to do these includes differently depending on which version of gcc
  * we're compiling with
+ *
+ * In G++ 4.3, hash_set and hash_map were formally deprecated and
+ * moved from ext/ to backward/.  Well, that's what the release notes
+ * claim.  In fact, on my system, hash_set and hash_map appear in both
+ * ext/ and backward/.  But, hash_fun.h is only in backward/, necessi-
+ * tating the NEWER_GCC macro.
+ *
+ * The real fix is to replace
+ *   hash_set with tr1::unordered_set in <tr1/unordered_set>
+ *   hash_map with tr1::unordered_map in <tr1/unordered_map>
  */
 #if (__GNUC__ == 3 && __GNUC_MINOR__ > 0) || (__GNUC__ > 3)
 #define NEW_GCC
+#endif
+
+#if (__GNUC__ == 4 && __GNUC_MINOR__ >= 3) || (__GNUC__ > 4)
+#define NEWER_GCC
 #endif
 
 #ifdef NEW_GCC
