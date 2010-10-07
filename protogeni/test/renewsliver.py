9d6e468cb9d55b1cd3001c94881a88b8abe043b0
Gary Wong <gtw@flux.utah.edu>
2010-10-07 16:22:22 -0600

    Send the valid_until time to the CM (as well as the SA).

--- protogeni/test/renewsliver.py
+++ protogeni/test/renewsliver.py
@@ -82,6 +82,7 @@ print "Got the slice credential, renewing the sliver";
 params = {}
 params["credentials"]  = (slicecred,)
 params["slice_urn"]    = SLICEURN
+params["valid_until"]  = valid_until
 rval,response = do_method("cm", "RenewSlice", params, version="2.0")
 if rval:
     Fatal("Could not renew sliver")
