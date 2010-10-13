a773d833d620ca994439d980cda5c50e0434a5c1
Robert Ricci <ricci@cs.utah.edu>
2010-10-13 13:11:28 -0600

    Fifth attempt at fixing fixed interfaces in assign
    Think I got it this time (yeah, I've said that four times before).
    It requires some extra bookkeeping; we were recording source and
    dest interface names in tb_plink, but not source and dest node
    names. With this new information, find_best_link() now has sole
    responsibility for figuring out if the vlink and plink go in the
    same direction, and the callers of find_best_link() are now
    responsible for knowing which direction they are traversing the
    vlink in (and thus whether they should tell find_best_link() to
    check the source or dest of the vlink.)

--- assign/parse_ptop.cc
+++ assign/parse_ptop.cc
@@ -269,8 +269,8 @@ int parse_ptop(tb_pgraph &pg, tb_sgraph &sg, istream& input)
       for (int cur = 0;cur<num;++cur) {
 	pedge pe = (add_edge(srcv,dstv,pg)).first;
 	tb_plink *pl = new
-	    tb_plink(name,tb_plink::PLINK_NORMAL,link_type,srcmac,dstmac,
-		     srciface,dstiface);
+	    tb_plink(name,tb_plink::PLINK_NORMAL,link_type,srcnode->name,srcmac,
+		     srciface,dstnode->name,dstmac,dstiface);
 	put(pedge_pmap,pe,pl);
 	pl->delay_info.bandwidth = ibw;
 	pl->delay_info.delay = idelay;
