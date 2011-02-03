be7c5fe212b6610ba200e60b6cb45c1cfc2ead8e
Mike Hibler <hibler@cs.utah.edu>
2011-02-03 12:31:24 -0700

    Require tmcd version 33 for "doblobs".
    This way I don't have to bother with backward compat for frisbee distribution
    of blobs.

--- tmcd/tmcd.c
+++ tmcd/tmcd.c
@@ -3250,6 +3250,18 @@ COMMAND_PROTOTYPE(doblobs)
 	char            buf[MYBUFSIZE];
 	char            *bufp = buf, *ebufp = &buf[sizeof(buf)];
 	
+	/*
+	 * Frisbee blobs did exist prior to version 33, but the infrastructure
+	 * was not deployed on more than a couple of images and only at Utah.
+	 * So we are not going to bother with frisbee backward compat changes
+	 * and just require that all images have the new frisbee master server
+	 * infrastructure.
+	 */
+	if (vers < 33) {
+		error("BLOBS: %s: requires new frisbee, rebuild image!\n",
+		      reqp->nodeid);
+		return 1;
+	}
 	res = mydb_query("select path,action from experiment_blobs "
 			 " where exptidx=%d order by idx",
 			 2, reqp->exptidx);
