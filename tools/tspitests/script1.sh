fc89eb38451c01b5d12e4accfea7cd4647587ba6
Mike Hibler <hibler@cs.utah.edu>
2011-10-08 22:13:38 -0600

    Adjust the set of unix gids used for a download server.
    When downloading an image, start the frisbeed process with the minimum set of
    gids necessary to access the image. This includes the unix gid of the
    project that the image is in and, optionally, the unix gid of the project
    subgroup if the image is part of one.
    
    Previously, we just use the gid set of the uid of the swapper of the
    experiment. Not only was this excessive, but it might also not include the
    gids needed in the case of a "global" image that is not in the world-readable
    /usr/testbed/images directory.

--- tools/tspitests/script1.sh
+++ tools/tspitests/script1.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-
+#
 # EMULAB-COPYRIGHT
-# Copyright (c) 2010 University of Utah and the Flux Group.
+# Copyright (c) 2010-2011 University of Utah and the Flux Group.
 # All rights reserved.
+#
 
 MP=`which modprobe`
 KILLALL=`which killall`
@@ -12,16 +13,35 @@ TMCC=`which tmcc`
 SLEEPTIME=1
 
 # Error check later
+echo "Doing TPM setup ..."
 /etc/testbed/rc/rc.tpmsetup
 
 ${MP} tpm_tis
 ${KILLALL} -9 tcsd; sleep ${SLEEPTIME}
 ${TCSD}
 
-SSCRUFT=`${TMCC} quoteprep RELOADSETUP | ${DOQ} RELOADSETUP`
-${TMCC} securestate ${SSCRUFT}
+echo "Requesting info for RELOADSETUP quote ..."
+QINFO=`${TMCC} quoteprep RELOADSETUP`
+if [ -z "$QINFO" ]; then
+    echo "*** could not get RELOADSETUP quote info"
+    exit 1
+fi
+
+echo "Preparing RELOADSETUP quote ..."
+SSCRUFT=`echo $QINFO | ${DOQ} RELOADSETUP`
+if [ -z "$SSCRUFT" ]; then
+    echo "*** could not produce RELOADSETUP quote"
+    exit 1
+fi
+
+echo "Sending RELOADSETUP quote ..."
+RC=`${TMCC} securestate ${SSCRUFT}`
+if [ $? -ne 0 -o "$RC" = "FAILED" ]; then
+    echo "*** could not transition to RELOADSETUP"
+    exit 1
+fi
 
 ${KILLALL} -9 tcsd; sleep ${SLEEPTIME}
 ${TCSD}
-${TMCC} -T imagekey > /tmp/secureloadinfo.out
 
+exit 0
--- tools/tspitests/script2.sh
+++ tools/tspitests/script2.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-
+#
 # EMULAB-COPYRIGHT
-# Copyright (c) 2010 University of Utah and the Flux Group.
+# Copyright (c) 2010-2011 University of Utah and the Flux Group.
 # All rights reserved.
+#
 
 KILLALL=`which killall`
 TCSD=`which tcsd`
@@ -15,6 +16,7 @@ REBOOTPCR=15
 
 # Error check later
 
+echo "Setting sign-off PCR ..."
 ${KILLALL} -9 tcsd; sleep ${SLEEPTIME}
 ${TCSD}
 ${TPMS} ${REBOOTPCR}
@@ -22,6 +24,25 @@ ${TPMS} ${REBOOTPCR}
 ${KILLALL} -9 tcsd; sleep ${SLEEPTIME}
 ${TCSD}
 
-SSCRUFT=`${TMCC} quoteprep TPMSIGNOFF | ${DOQ} TPMSIGNOFF`
-${TMCC} securestate ${SSCRUFT}
+echo "Requesting info for TPMSIGNOFF quote ..."
+QINFO=`${TMCC} quoteprep TPMSIGNOFF`
+if [ -z "$QINFO" ]; then
+    echo "*** could not get TPMSIGNOFF quote info"
+    exit 1
+fi
 
+echo "Preparing TPMSIGNOFF quote ..."
+SSCRUFT=`echo $QINFO | ${DOQ} TPMSIGNOFF`
+if [ -z "$SSCRUFT" ]; then
+    echo "*** could not produce TPMSIGNOFF quote"
+    exit 1
+fi
+
+echo "Sending TPMSIGNOFF quote ..."
+RC=`${TMCC} securestate ${SSCRUFT}`
+if [ $? -ne 0 -o "$RC" = "FAILED" ]; then
+    echo "*** could not transition to TPMSIGNOFF"
+    exit 1
+fi
+
+exit 0
